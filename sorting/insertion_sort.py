dfa8c302dc82a13ce8a0db0600531c29c4098ec6
Samuel Huang <samhuang91@gmail.com>
2017-05-26 08:49:40 -0700

    Fix insertion sort

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -3,8 +3,9 @@
 
 # https://en.wikipedia.org/wiki/Insertion_sort
 def sort(arr):
-    for i, ival in enumerate(arr):
-        item = ival
+    alen = len(arr)
+    for i in range(1, alen):
+        item = arr[i]
         j = i - 1
         while j >= 0 and item < arr[j]:
             arr[j + 1] = arr[j]
