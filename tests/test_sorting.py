a81082e52fa9a32485dd498743525d9872b27730
Samuel Huang <samhuang91@gmail.com>
2017-05-30 21:20:57 -0700

    Place all tests into one file

---
+++ tests/test_sorting.py
@@ -0,0 +1,42 @@
+from sorting import (
+    bubble_sort,
+    insertion_sort,
+    merge_sort,
+    quick_sort,
+    selection_sort,
+)
+
+
+class TestSorting(object):
+
+    @staticmethod
+    def _is_sorted(array):
+        alen = len(array)
+        for i in range(1, alen):
+            if array[i-1] > array[i]:
+                return False
+        return True
+
+    def test_bubble_sort(self, array):
+        bubble_sort.sort(array)
+        assert self._is_sorted(array)
+
+    def test_insertion_sort(self, array):
+        insertion_sort.sort(array)
+        assert self._is_sorted(array)
+
+    def test_merge_sort(self, array):
+        merge_sort.sort(array)
+        assert self._is_sorted(array)
+
+    def test_quick_sort_iterative(self, array):
+        quick_sort.sort(array)
+        assert self._is_sorted(array)
+
+    def test_quick_sort_recursive(self, array):
+        quick_sort.sort(array, iterative=False)
+        assert self._is_sorted(array)
+
+    def test_selection_sort(self, array):
+        selection_sort.sort(array)
+        assert self._is_sorted(array)
