ced72f9793266af07ed877ba2727252de52f80fc
Samuel Huang <samhuang91@gmail.com>
2018-08-23 22:34:05 -0700

    Move sorting tests into multiple files

---
+++ tests/sorting/test_selection_sort.py
@@ -0,0 +1,17 @@
+from sorting import selection_sort
+
+
+class TestSort(object):
+
+    @staticmethod
+    def _is_sorted(array):
+        alen = len(array)
+        for i in range(1, alen):
+            if array[i-1] > array[i]:
+                return False
+        return True
+
+    def test_selection_sort(self, array):
+        assert not self._is_sorted(array)
+        selection_sort.sort(array)
+        assert self._is_sorted(array)
