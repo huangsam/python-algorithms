5ef77e7b74cb3529d08609f2e69608b55bdf01f1
Samuel Huang <samhuang91@gmail.com>
2018-08-23 23:10:49 -0700

    Moving asserts under tests

---
+++ tests/basic/test_check_anagram.py
@@ -0,0 +1,13 @@
+from basic.anagram import check_anagram
+
+
+class TestAnagram(object):
+
+    def test_check_anagram_good(self):
+        assert check_anagram("dog", "god") is True
+        assert check_anagram("mom", "mom") is True
+        assert check_anagram("cinema", "iceman") is True
+
+    def test_check_anagram_bad(self):
+        assert check_anagram("gas", "gag") is False
+        assert check_anagram("gas", "gass") is False
