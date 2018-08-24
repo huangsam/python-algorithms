4a3744829aaa9e450fbdf328ae0f981dbe61c800
Samuel Huang <samhuang91@gmail.com>
2018-08-23 23:32:07 -0700

    Move all online resources under online

--- basic/square_root.py
+++ basic/square_root.py
@@ -1,19 +1,10 @@
-def square_root(x, error=0.05):
-    """Square root of x.
-
-    Args:
-        x: The input to compute the square root of.
-        error: The margin of acceptible error.
-
-    Returns:
-        The output estimated within two decimal places.
-    """
+def square_root(x, precision=0.05):
     start, end = 1.0, x
     while start < end:
         mid = (start + end) / 2.0
-        if mid * mid > x + error:
+        if mid * mid > x + precision:
             start, end = start, mid
-        elif mid * mid < x - error:
+        elif mid * mid < x - precision:
             start, end = mid, end
         else:
             return round(mid, 2)
