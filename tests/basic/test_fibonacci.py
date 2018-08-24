d79dad81826a6eaf21df8f7c552708dac3d876dd
Samuel Huang <samhuang91@gmail.com>
2018-08-23 23:19:06 -0700

    Handle more cases in fibonacci

--- tests/basic/test_fibonacci.py
+++ tests/basic/test_fibonacci.py
@@ -8,10 +8,16 @@ from basic.fibonacci import (
 class TestFibonacci(object):
 
     def test_fibonacci_recursive(self):
+        assert fibonacci_recursive(0) == 0
+        assert fibonacci_recursive(1) == 1
         assert fibonacci_recursive(4) == 3
 
     def test_fibonacci_iterative(self):
+        assert fibonacci_iterative(0) == 0
+        assert fibonacci_iterative(1) == 1
         assert fibonacci_iterative(4) == 3
 
     def test_fibonacci_stack(self):
+        assert fibonacci_stack(0) == 0
+        assert fibonacci_stack(1) == 1
         assert fibonacci_stack(4) == 3
