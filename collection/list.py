0d27ac3afb05858087504946f9147904fc68bd78
Samuel Huang <samhuang91@gmail.com>
2018-08-27 10:23:27 -0700

    Update and rename linked_list.py to list.py

--- collection/list.py
+++ collection/list.py
@@ -1,6 +1,7 @@
 class ListNode(object):
-    """List node has a value and reference to next node."""
+    """List node has a value and references to other nodes."""
 
     def __init__(self, value=None):
         self.value = value
         self.next_node = None
+        self.prev_node = None
