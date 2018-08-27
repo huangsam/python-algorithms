5d9b0422d63a1225d4794fbb8543746e9ea3b17c
Samuel Huang <samhuang91@gmail.com>
2018-08-27 10:20:38 -0700

    Create linked_list.py

---
+++ collection/linked_list.py
@@ -0,0 +1,6 @@
+class ListNode(object):
+    """List node has a value and reference to next node."""
+
+    def __init__(self, value=None):
+        self.value = value
+        self.next_node = None
