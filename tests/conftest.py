17175d73ecef64efad53df754da892d2ab7f4da0
Samuel Huang <samhuang91@gmail.com>
2018-08-27 23:30:17 -0700

    Add linked list tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@ from random import randint
 import pytest
 
 
-@pytest.fixture(scope="function",
-                params=[50, 100, 400])
+@pytest.fixture(scope='function', params=[50, 100, 400])
 def array(request):
     return [randint(0, 100) for i in range(request.param)]
