94d859a477fa89c4d841ba8b3b976b167f0b5cda
Samuel Huang <samhuang91@gmail.com>
2018-08-27 23:42:19 -0700

    Minimize logic needed

--- basic/reverse_list.py
+++ basic/reverse_list.py
@@ -1,12 +1,3 @@
-class ListNode(object):
-    """List node has a value and references to other nodes."""
-
-    def __init__(self, value=None):
-        self.value = value
-        self.next_node = None
-        self.prev_node = None
-
-
 def reverse_list(ll):
     cur_node = ll
     next_node = None
