e6e95f0befd2c4ba61c8e1d1b910f454c71433f2
Samuel Huang <samhuang91@gmail.com>
2018-09-01 01:59:22 -0700

    Add iterative version

--- basic/bsearch.py
+++ basic/bsearch.py
@@ -8,3 +8,17 @@ def bsearch_recursive(arr, t, lo, hi):
     elif val < t:
         return bsearch_recursive(arr, t, mid + 1, hi)
     return bsearch_recursive(arr, t, lo, mid)
+
+
+def bsearch_iterative(arr, t):
+    lo, hi = 0, len(arr)
+    while lo < hi:
+        mid = (lo + hi) // 2
+        val = arr[mid]
+        if val == t:
+            return True
+        elif val < t:
+            lo, hi = mid + 1, hi
+        else:
+            lo, hi = lo, mid
+    return False
--- tests/basic/test_bsearch.py
+++ tests/basic/test_bsearch.py
@@ -1,22 +1,31 @@
-from basic.bsearch import bsearch_recursive
+from basic.bsearch import (
+    bsearch_recursive,
+    bsearch_iterative,
+)
 
 
 class TestBinarySearch(object):
 
     def test_bsearch_recursive_good(self):
         arr = [0, 2, 4, 6, 8, 10]
-        result = bsearch_recursive(arr, 4, 0, len(arr))
-        assert result is True
-        result = bsearch_recursive(arr, 0, 0, len(arr))
-        assert result is True
-        result = bsearch_recursive(arr, 10, 0, len(arr))
-        assert result is True
+        assert bsearch_recursive(arr, 4, 0, len(arr)) is True
+        assert bsearch_recursive(arr, 0, 0, len(arr)) is True
+        assert bsearch_recursive(arr, 10, 0, len(arr)) is True
 
     def test_bsearch_recursive_bad(self):
         arr = [0, 2, 4, 6, 8, 10]
-        result = bsearch_recursive(arr, 3, 0, len(arr))
-        assert result is False
-        result = bsearch_recursive(arr, 1, 0, len(arr))
-        assert result is False
-        result = bsearch_recursive(arr, 11, 0, len(arr))
-        assert result is False
+        assert bsearch_recursive(arr, 3, 0, len(arr)) is False
+        assert bsearch_recursive(arr, 1, 0, len(arr)) is False
+        assert bsearch_recursive(arr, 11, 0, len(arr)) is False
+
+    def test_bsearch_iterative_good(self):
+        arr = [0, 2, 4, 6, 8, 10]
+        assert bsearch_iterative(arr, 4) is True
+        assert bsearch_iterative(arr, 0) is True
+        assert bsearch_iterative(arr, 10) is True
+
+    def test_bsearch_iterative_bad(self):
+        arr = [0, 2, 4, 6, 8, 10]
+        assert bsearch_iterative(arr, 3) is False
+        assert bsearch_iterative(arr, 1) is False
+        assert bsearch_iterative(arr, 11) is False
