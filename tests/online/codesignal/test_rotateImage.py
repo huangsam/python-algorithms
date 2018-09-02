9d980821a0a17c8b0645fb389277664b149f2c13
Samuel Huang <samhuang91@gmail.com>
2018-09-01 22:12:00 -0700

    Add more rotate test cases

--- tests/online/codesignal/test_rotateImage.py
+++ tests/online/codesignal/test_rotateImage.py
@@ -3,6 +3,26 @@ from online.codesignal.rotateImage import rotateImage
 
 class TestRotateImage(object):
 
+    def test_rotate_one(self):
+        image = [
+            [1],
+        ]
+        rotated = [
+            [1],
+        ]
+        assert rotated == rotateImage(image)
+
+    def test_rotate_two(self):
+        image = [
+            [1, 1],
+            [2, 2],
+        ]
+        rotated = [
+            [2, 1],
+            [2, 1],
+        ]
+        assert rotated == rotateImage(image)
+
     def test_rotate_three(self):
         image = [
             [1, 1, 1],
@@ -30,26 +50,5 @@ class TestRotateImage(object):
             [5, 4, 3, 2, 1],
             [5, 4, 3, 2, 1],
             [5, 4, 3, 2, 1],
-        ]
-        assert rotated == rotateImage(image)
-
-    def test_rotate_seven(self):
-        image = [
-            [1, 1, 1, 1, 1, 1, 1],
-            [2, 2, 2, 2, 2, 2, 2],
-            [3, 3, 3, 3, 3, 3, 3],
-            [4, 4, 4, 4, 4, 4, 4],
-            [5, 5, 5, 5, 5, 5, 5],
-            [6, 6, 6, 6, 6, 6, 6],
-            [7, 7, 7, 7, 7, 7, 7],
-        ]
-        rotated = [
-            [7, 6, 5, 4, 3, 2, 1],
-            [7, 6, 5, 4, 3, 2, 1],
-            [7, 6, 5, 4, 3, 2, 1],
-            [7, 6, 5, 4, 3, 2, 1],
-            [7, 6, 5, 4, 3, 2, 1],
-            [7, 6, 5, 4, 3, 2, 1],
-            [7, 6, 5, 4, 3, 2, 1],
         ]
         assert rotated == rotateImage(image)
