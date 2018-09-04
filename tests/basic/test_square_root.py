e8a41b9ef558d16ab9eda8d1ffe89a6077e2dfe9
Samuel Huang <samhuang91@gmail.com>
2018-09-03 22:47:55 -0700

    Update test_square_root.py

--- tests/basic/test_square_root.py
+++ tests/basic/test_square_root.py
@@ -5,7 +5,7 @@ from basic.square_root import square_root
 
 class TestSquareRoot(object):
 
-    @pytest.parametrize("root, x", [
+    @pytest.mark.parametrize("root, x", [
         (9.0, 81),
         (12.0, 144),
         (13.0, 169),
@@ -13,7 +13,7 @@ class TestSquareRoot(object):
     def test_square_root_exact(self, root, x):
         assert root == square_root(x)
 
-    @pytest.parametrize("left, right, x", [
+    @pytest.mark.parametrize("left, right, x", [
         (14.0, 15.0, 200),
         (12.0, 13.0, 130),
         (2.0, 3.0, 7),
