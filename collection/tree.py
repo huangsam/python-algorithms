0e080c34970af9aacef936e5ee84255254ea83f8
Samuel Huang <samhuang91@gmail.com>
2018-09-05 16:48:36 -0700

    Update tree.py

--- collection/tree.py
+++ collection/tree.py
@@ -5,3 +5,11 @@ class TreeNode(object):
         self.val = x
         self.left = None
         self.right = None
+
+    def insert_left(self, val):
+        self.left = TreeNode(val)
+        return self.left
+
+    def insert_right(self, val):
+        self.right = TreeNode(val)
+        return self.right
