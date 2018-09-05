23c9e712b476b7180585fe296d34a97ca1a45d33
Samuel Huang <samhuang91@gmail.com>
2018-09-04 23:44:09 -0700

    Add bst array tests

---
+++ tests/online/glassdoor/test_bst_array.py
@@ -0,0 +1,27 @@
+import pytest
+
+from graph.inorder import inorder_recursive
+from online.glassdoor.bst_array import (
+    create_bst_from_array,
+    is_sorted,
+)
+
+
+class TestBSTArray(object):
+
+    @pytest.mark.parametrize("given, expected", [
+        ([1], 1),
+        ([1, 2, 3], 2),
+        ([6, 8, 10], 8),
+        ([1, 2, 3, 4, 5], 3),
+        ([1, 2, 3, 4, 5, 6, 7], 4),
+    ])
+    def test_create_bst_from_array(self, given, expected):
+        root = create_bst_from_array(given, 0, len(given) - 1)
+        assert root.val == expected
+        val, flag = is_sorted(root)
+        assert val == root.val and flag is True
+        visited = inorder_recursive(root)
+        assert len(visited) == len(given)
+        for v, g in zip(visited, given):
+            assert v == g
