c1d6d0433eef3673e7126333626bc6f7e7e38b81
Samuel Huang <samhuang91@gmail.com>
2018-09-05 22:03:02 -0700

    Refine inputs for is_balanced

--- tests/online/cake/test_is_balanced.py
+++ tests/online/cake/test_is_balanced.py
@@ -8,25 +8,25 @@ from online.cake.is_balanced import (
 class TestIsBalanced(object):
 
     def test_is_balanced(self):
-        root = TreeNode(6)
-        root.insert_left(3)
-        root.insert_right(4)
-        root.left.insert_left(2)
-        root.left.insert_right(7)
+        root = TreeNode(1)
+        root.insert_left(2)
+        root.insert_right(3)
+        root.left.insert_left(4)
+        root.left.insert_right(5)
         assert is_balanced(root) is True
 
     def test_is_balanced_optimal_good(self):
-        root = TreeNode(6)
-        root.insert_left(3)
-        root.insert_right(4)
-        root.left.insert_left(2)
-        root.left.insert_right(7)
+        root = TreeNode(1)
+        root.insert_left(2)
+        root.insert_right(3)
+        root.left.insert_left(4)
+        root.left.insert_right(5)
         balanced, _ = is_balanced_optimal(root)
         assert balanced is True
 
     def test_is_balanced_optimal_bad(self):
-        root = TreeNode(6)
-        root.insert_left(3)
-        root.left.insert_right(7)
+        root = TreeNode(1)
+        root.insert_left(2)
+        root.left.insert_right(5)
         balanced, _ = is_balanced_optimal(root)
         assert balanced is False
