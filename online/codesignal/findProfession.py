5607ec561f8518e1e77b275e8fdfa684c9caf9cf
Samuel Huang <samhuang91@gmail.com>
2018-09-05 23:35:58 -0700

    Add findProfession solutiong

---
+++ online/codesignal/findProfession.py
@@ -0,0 +1,9 @@
+def findProfession(level, pos):
+    bstream = bin(pos - 1)[2:]
+    ones = 0
+    for bit in bstream:
+        if bit == '1':
+            ones += 1
+    if ones % 2 == 0:
+        return 'Engineer'
+    return 'Doctor'
