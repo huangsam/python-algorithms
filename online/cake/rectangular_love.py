188a3150376ab2f8ae61fd65bae35ef726bbca4e
Samuel Huang <samhuang91@gmail.com>
2018-09-05 21:56:08 -0700

    Fixup rectangular_love

--- online/cake/rectangular_love.py
+++ online/cake/rectangular_love.py
@@ -6,16 +6,14 @@ class Rectangle(object):
 
 
 def rectangular_love(r1, r2):
-    if not has_overlap(r1.top, r1.bottom, r2.top, r2.bottom):
+    if not has_overlap(r1.bottom, r1.top, r2.bottom, r2.top):
         return False
     if not has_overlap(r1.left, r1.right, r2.left, r2.right):
         return False
     return True
 
 
-def has_overlap(l1_bound, r1_bound, l2_bound, r2_bound):
-    if l1_bound < r1_bound < l2_bound < r2_bound:
-        return False
-    if l2_bound < r2_bound < l1_bound < r1_bound:
+def has_overlap(a_min, a_max, b_min, b_max):
+    if (a_min > b_max) or (a_max < b_min):
         return False
     return True
