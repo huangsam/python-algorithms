6ff8068e03aa3d7a1b977e12d03ff7971c24706c
Samuel Huang <samhuang91@gmail.com>
2018-09-05 18:43:47 -0700

    Update is_balanced.py

--- online/cake/is_balanced.py
+++ online/cake/is_balanced.py
@@ -12,3 +12,15 @@ def is_balanced(root):
     return abs(left_depth - right_depth) <= 1 \
         and is_balanced(root.left) \
         and is_balanced(root.right)
+
+
+def is_balanced_optimal(root):
+    if root is None:
+        return (True, 0)
+    left_b, left_h = is_balanced_optimal(root.left)
+    right_b, right_h = is_balanced_optimal(root.right)
+    height = 1 + max(left_h, right_h)
+    h_diff = abs(left_h - right_h)
+    if h_diff > 1 or left_b is False or right_b is False:
+        return (False, -1)
+    return (True, height)
