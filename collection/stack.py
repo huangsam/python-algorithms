c24863431ac3366b211da27d6d5982dbba70769d
Samuel Huang <samhuang91@gmail.com>
2018-09-05 22:20:43 -0700

    Add max routine for stack

--- collection/stack.py
+++ collection/stack.py
@@ -3,12 +3,23 @@ class Stack(object):
 
     def __init__(self):
         self.array = []
+        self.maxval = []
+
+    def max(self):
+        if len(self.maxval) == 0:
+            raise ValueError('No entries have been added')
+        return self.maxval[-1]
 
     def push(self, val):
         self.array.append(val)
+        if len(self.maxval) == 0 or self.maxval[-1] <= val:
+            self.maxval.append(val)
 
     def pop(self):
-        return self.array.pop()
+        val = self.array.pop()
+        if len(self.maxval) > 0 and self.maxval[-1] == val:
+            self.maxval.pop()
+        return val
 
     def size(self):
         return len(self.array)
