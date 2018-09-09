a5dd68202125bf882a1e88c7c67b0c36b1ec8d96
Samuel Huang <samhuang91@gmail.com>
2018-09-08 23:29:04 -0700

    Convert pair_sum main to tests

--- online/youtube/pair_sum.py
+++ online/youtube/pair_sum.py
@@ -18,26 +18,3 @@ def pair_sum(arr, target):
         diff = target - val
         ind_to_diff[diff] = val
     return pairs
-
-
-def main():
-    target = 10
-    array = [3, 7, 10, 9, 1, 5]
-    result = pair_sum(array, target)
-    assert len(result) == 2
-    assert (5, 5) not in result
-    assert (7, 3) in result
-    assert (1, 9) in result
-
-    target = 15
-    result = pair_sum(array, target)
-    assert len(result) == 1
-    assert (5, 10) in result
-
-    target = 2
-    result = pair_sum(array, target)
-    assert len(result) == 0
-
-
-if __name__ == '__main__':
-    main()
---
+++ tests/online/youtube/test_pair_sum.py
@@ -0,0 +1,30 @@
+import pytest
+
+from online.youtube.pair_sum import pair_sum
+
+
+@pytest.fixture(scope='function')
+def array():
+    return [3, 7, 10, 9, 1, 5]
+
+
+class TestPairSum(object):
+
+    def test_pair_sum_multiple(self, array):
+        target = 10
+        result = pair_sum(array, target)
+        assert len(result) == 2
+        assert (5, 5) not in result
+        assert (7, 3) in result
+        assert (1, 9) in result
+
+    def test_pair_sum_single(self, array):
+        target = 15
+        result = pair_sum(array, target)
+        assert len(result) == 1
+        assert (5, 10) in result
+
+    def test_pair_sum_none(self, array):
+        target = 2
+        result = pair_sum(array, target)
+        assert len(result) == 0
