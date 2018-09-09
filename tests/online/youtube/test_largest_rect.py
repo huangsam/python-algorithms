83eefbaf03985c52ced62a681a9ad792491d2c7d
Samuel Huang <samhuang91@gmail.com>
2018-09-08 22:26:59 -0700

    Convert largest_rect main to tests

---
+++ tests/online/youtube/test_largest_rect.py
@@ -0,0 +1,14 @@
+import pytest
+
+from online.youtube.largest_rect import largest_rect
+
+
+class TestLargestRect(object):
+
+    @pytest.mark.parametrize('histo, expected', [
+        ([1, 3, 5, 3, 0, 2, 6, 6, 1, 0, 3, 6], 12),
+        ([1, 3, 5, 3, 2, 2, 3, 3, 1, 0, 3, 6], 14),
+        ([1, 3, 2, 1, 2], 5),
+    ])
+    def test_largest_rect(self, histo, expected):
+        assert largest_rect(histo) == expected
