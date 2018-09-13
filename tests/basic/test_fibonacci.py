9efa8b46a8fbc69fd28753b2db08ca9e0b2bf160
Samuel Huang <samhuang91@gmail.com>
2018-09-12 23:00:21 -0700

    Implement fibonacci_dp_top

--- tests/basic/test_fibonacci.py
+++ tests/basic/test_fibonacci.py
@@ -5,6 +5,7 @@ from basic.fibonacci import (
     fibonacci_iterative,
     fibonacci_stack,
     fibonacci_dp_bottom,
+    fibonacci_dp_top,
 )
 
 
@@ -18,7 +19,11 @@ class TestFibonacci(object):
     @pytest.mark.parametrize('i,o', [
         (0, 0),
         (1, 1),
+        (2, 1),
+        (3, 2),
         (4, 3),
+        (5, 5),
+        (6, 8),
     ])
     def test_fibonacci(self, func, i, o):
         assert func(i) == o
