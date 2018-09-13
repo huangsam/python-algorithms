51e6f084c90e677d37d5378db4fa9e744e67f33a
Samuel Huang <samhuang91@gmail.com>
2018-09-13 11:49:51 -0700

    Add tests for said removals

---
+++ tests/online/glassdoor/test_remove_bad_edge.py
@@ -0,0 +1,16 @@
+import pytest
+
+from collection.tree import TreeNode
+from online.glassdoor.remove_bad_edge import remove_bad_edge
+
+
+class TestRemoveBadEdge(object):
+
+    def test_remove_bad_edge(self):
+        root = TreeNode(6)
+        root.left = TreeNode(3)
+        root.right = TreeNode(8)
+        root.left.left = TreeNode(2)
+        root.left.left.right = TreeNode(8)
+        remove_bad_edge(root)
+        assert root.right is None
---
+++ tests/online/glassdoor/test_remove_every_other.py
@@ -0,0 +1,26 @@
+import pytest
+
+from collection.list import ListNode
+from online.glassdoor.remove_every_other import remove_every_other
+
+
+class TestRemoveEveryOther(object):
+
+    @staticmethod
+    def _add_loop(l):
+        prev = None
+        cur = l
+        while cur:
+            prev = cur
+            cur = cur.next_node
+        prev.next_node = l
+
+    def test_remove_every_other(self, sorted_list):
+        self._add_loop(sorted_list)
+        remove_every_other(sorted_list)
+        cur = sorted_list
+        while cur:
+            assert cur.value % 2 == 0
+            cur = cur.next_node
+            if cur is sorted_list:
+                return
