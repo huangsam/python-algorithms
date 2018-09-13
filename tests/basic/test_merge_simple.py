20a10314a8e1e5a5dab954eb03c042d96d35a334
Samuel Huang <samhuang91@gmail.com>
2018-09-12 21:18:34 -0700

    Add tests for merge_simple

---
+++ tests/basic/test_merge_simple.py
@@ -0,0 +1,17 @@
+from basic.merge_simple import merge_sort
+
+
+class TestSort(object):
+
+    @staticmethod
+    def _is_sorted(array):
+        alen = len(array)
+        for i in range(1, alen):
+            if array[i-1] > array[i]:
+                return False
+        return True
+
+    def test_merge_simple(self, array):
+        assert not self._is_sorted(array)
+        array = merge_sort(array)
+        assert self._is_sorted(array)
