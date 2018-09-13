94a1606e18c3108326a0c14a66956161ea71e932
Samuel Huang <samhuang91@gmail.com>
2018-09-12 22:12:00 -0700

    Fix bsearch and refine tests

--- basic/bsearch.py
+++ basic/bsearch.py
@@ -1,18 +1,22 @@
-def bsearch_recursive(arr, t, lo, hi):
-    if lo >= hi:
+def bsearch_recursive(arr, t):
+    return bsearch_recursive_work(arr, t, 0, len(arr) - 1)
+
+
+def bsearch_recursive_work(arr, t, lo, hi):
+    if lo > hi:
         return False
     mid = (lo + hi) // 2
     val = arr[mid]
     if val == t:
         return True
     elif val < t:
-        return bsearch_recursive(arr, t, mid + 1, hi)
-    return bsearch_recursive(arr, t, lo, mid)
+        return bsearch_recursive_work(arr, t, mid + 1, hi)
+    return bsearch_recursive_work(arr, t, lo, mid - 1)
 
 
 def bsearch_iterative(arr, t):
-    lo, hi = 0, len(arr)
-    while lo < hi:
+    lo, hi = 0, len(arr) - 1
+    while lo <= hi:
         mid = (lo + hi) // 2
         val = arr[mid]
         if val == t:
@@ -20,5 +24,5 @@ def bsearch_iterative(arr, t):
         elif val < t:
             lo, hi = mid + 1, hi
         else:
-            lo, hi = lo, mid
+            lo, hi = lo, mid - 1
     return False
