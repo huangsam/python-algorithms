496723b4d2019c751492899451ba0835e558d21f
Samuel Huang <samhuang91@gmail.com>
2018-09-12 22:12:00 -0700

    Fix bsearch and refine tests

--- tests/basic/test_bsearch.py
+++ tests/basic/test_bsearch.py
@@ -1,3 +1,5 @@
+import pytest
+
 from basic.bsearch import (
     bsearch_recursive,
     bsearch_iterative,
@@ -6,26 +8,14 @@ from basic.bsearch import (
 
 class TestBinarySearch(object):
 
-    def test_bsearch_recursive_good(self):
+    @pytest.mark.parametrize('i', [4, 0, 10])
+    @pytest.mark.parametrize('func', [bsearch_iterative, bsearch_recursive])
+    def test_bsearch_good(self, func, i):
         arr = [0, 2, 4, 6, 8, 10]
-        assert bsearch_recursive(arr, 4, 0, len(arr)) is True
-        assert bsearch_recursive(arr, 0, 0, len(arr)) is True
-        assert bsearch_recursive(arr, 10, 0, len(arr)) is True
+        assert func(arr, i) is True
 
-    def test_bsearch_recursive_bad(self):
+    @pytest.mark.parametrize('i', [3, 1, 11])
+    @pytest.mark.parametrize('func', [bsearch_iterative, bsearch_recursive])
+    def test_bsearch_bad(self, func, i):
         arr = [0, 2, 4, 6, 8, 10]
-        assert bsearch_recursive(arr, 3, 0, len(arr)) is False
-        assert bsearch_recursive(arr, 1, 0, len(arr)) is False
-        assert bsearch_recursive(arr, 11, 0, len(arr)) is False
-
-    def test_bsearch_iterative_good(self):
-        arr = [0, 2, 4, 6, 8, 10]
-        assert bsearch_iterative(arr, 4) is True
-        assert bsearch_iterative(arr, 0) is True
-        assert bsearch_iterative(arr, 10) is True
-
-    def test_bsearch_iterative_bad(self):
-        arr = [0, 2, 4, 6, 8, 10]
-        assert bsearch_iterative(arr, 3) is False
-        assert bsearch_iterative(arr, 1) is False
-        assert bsearch_iterative(arr, 11) is False
+        assert func(arr, i) is False
