4f80a59c56cbfce7823534ad99e50634d37f56ff
Samuel Huang <samhuang91@gmail.com>
2018-09-15 16:46:31 -0700

    Use for loop instead

--- online/glassdoor/product_array.py
+++ online/glassdoor/product_array.py
@@ -2,11 +2,9 @@
 def product_array(arr):
     l_mult, r_mult = 1, 1
     new_arr = [1] * len(arr)
-    i = 1
-    while i < len(arr):
+    for i in range(1, len(arr)):
         l_mult *= arr[i-1]
         new_arr[i] *= l_mult
         r_mult *= arr[len(arr)-i]
         new_arr[len(arr)-i-1] *= r_mult
-        i += 1
     return new_arr
