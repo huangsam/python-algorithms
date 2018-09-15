a20457d4f956a284581d6f42126c79c3d9e6e44e
Samuel Huang <samhuang91@gmail.com>
2018-09-15 16:45:36 -0700

    Implement product_array

---
+++ tests/online/glassdoor/test_product_array.py
@@ -0,0 +1,15 @@
+import pytest
+
+from online.glassdoor.product_array import product_array
+
+
+class TestProductArray(object):
+
+    @pytest.mark.parametrize('old, expected', [
+        ([1, 2, 3, 4, 5], [120, 60, 40, 30, 24]),
+        ([10, 3, 5, 6, 2], [180, 600, 360, 300, 900]),
+    ])
+    def test_product_array(self, old, expected):
+        new_arr = product_array(old)
+        for i, j in zip(new_arr, expected):
+            assert i == j
