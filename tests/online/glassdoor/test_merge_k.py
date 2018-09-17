2bc718ebb5f493913d5c71ffd1c0e86611ee9e9a
Samuel Huang <samhuang91@gmail.com>
2018-09-16 18:36:11 -0700

    Add test cases for helper function

--- tests/online/glassdoor/test_merge_k.py
+++ tests/online/glassdoor/test_merge_k.py
@@ -3,6 +3,7 @@ import pytest
 from online.glassdoor.merge_k import (
     merge_k_queue,
     merge_k_heap,
+    merge_two_arrays,
 )
 
 
@@ -37,5 +38,16 @@ class TestMergeK(object):
     @pytest.mark.parametrize('func', [merge_k_queue, merge_k_heap])
     def test_merge_k(self, arrays, expected, func):
         result = func(arrays)
+        for i, j in zip(result, expected):
+            assert i == j
+
+    @pytest.mark.parametrize('arr1, arr2, expected', [
+        ([10, 15, 30], [12, 15, 20], [10, 12, 15, 15, 20, 30]),
+        ([], [], []),
+        ([1], [], [1]),
+        ([1, 2, 3], [1, 5], [1, 1, 2, 3, 5]),
+    ])
+    def test_merge_two_arrays(self, arr1, arr2, expected):
+        result = merge_two_arrays(arr1, arr2)
         for i, j in zip(result, expected):
             assert i == j
