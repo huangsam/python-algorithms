9f2cb060c3e0e247a6b212dfd373425a329bd3fe
Samuel Huang <samhuang91@gmail.com>
2018-09-16 18:24:23 -0700

    Provide heap alternative

--- online/glassdoor/merge_k.py
+++ online/glassdoor/merge_k.py
@@ -1,4 +1,7 @@
-def merge_k(arrays):
+import heapq
+
+
+def merge_k_queue(arrays):
     while len(arrays) >= 2:
         arr1 = arrays.pop(0)
         arr2 = arrays.pop(0)
@@ -22,4 +25,26 @@ def merge(arr1, arr2):
         result.extend(arr2[i2:])
     elif i2 >= len(arr2):
         result.extend(arr1[i1:])
+    return result
+
+
+# https://www.geeksforgeeks.org/merge-k-sorted-arrays/
+def merge_k_heap(arrays):
+    result = []
+
+    heap = [(arr[0], i, 0) for i, arr in enumerate(arrays) if arr]
+    heapq.heapify(heap)
+
+    while heap:
+        val, arr_ind, element_ind = heapq.heappop(heap)
+
+        result.append(val)
+
+        if element_ind + 1 < len(arrays[arr_ind]):
+            next_tuple = (
+                arrays[arr_ind][element_ind + 1],
+                arr_ind,
+                element_ind + 1,
+            )
+            heapq.heappush(heap, next_tuple)
     return result
