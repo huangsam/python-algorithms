244a342c7ab9cadef6b6cfb7c7af0c673f469ff3
Samuel Huang <samhuang91@gmail.com>
2018-09-17 21:48:47 -0700

    Add depth_sum tests

---
+++ tests/online/glassdoor/test_depth_sum.py
@@ -0,0 +1,18 @@
+import pytest
+
+from online.glassdoor.depth_sum import (
+    depth_sum,
+    depth_sum_stack,
+)
+
+
+class TestDepthSum(object):
+
+    @pytest.mark.parametrize('nested, expected', [
+        ([[1, 1], 2, [1, 1]], 10),
+        ([[[3], [1,2]]], 18),
+        ([1, [2], [[3]]], 14),
+    ])
+    @pytest.mark.parametrize('func', [depth_sum, depth_sum_stack])
+    def test_depth_sum(self, nested, expected, func):
+        assert func(nested) == expected
