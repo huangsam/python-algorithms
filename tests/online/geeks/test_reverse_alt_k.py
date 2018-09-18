88be3aaecd1552a5de4e96c84af0579896d49d23
Samuel Huang <samhuang91@gmail.com>
2018-09-18 09:40:02 -0700

    Add reverse_alt_k tests

---
+++ tests/online/geeks/test_reverse_alt_k.py
@@ -0,0 +1,26 @@
+import pytest
+
+from collection.list import ListNode
+from online.geeks.reverse_alt_k import reverse_alt_k
+
+
+class TestReverseList(object):
+
+    @pytest.mark.parametrize('k, size, expected', [
+        (3, 6, [3, 2, 1, 4, 5, 6]),
+        (3, 9, [3, 2, 1, 4, 5, 6, 9, 8, 7])
+    ])
+    def test_reverse_alt_k(self, k, size, expected):
+        head = ListNode(1)
+        node = head
+        for i in range(2, size + 1):
+            node.next_node = ListNode(i)
+            node = node.next_node
+        nhead = reverse_alt_k(head, k)
+        result = []
+        node = nhead
+        i = 0
+        while node:
+            expected[i] = node.value
+            node = node.next_node
+            i += 1
