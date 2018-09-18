64eecc32fca1ea09346a5e6deb1b79592fa27348
Samuel Huang <samhuang91@gmail.com>
2018-09-17 21:07:46 -0700

    Add is_bst_optimal solution

--- online/glassdoor/is_bst.py
+++ online/glassdoor/is_bst.py
@@ -1,4 +1,6 @@
-# https://www.geeksforgeeks.org/a-program-to-check-if-a-binary-tree-is-bst-or-not/
+import math
+
+
 def is_bst(node):
     if node is None:
         return True
@@ -19,3 +21,17 @@ def get_min(node):
     while node.left:
         node = node.left
     return node.val
+
+
+# https://www.geeksforgeeks.org/a-program-to-check-if-a-binary-tree-is-bst-or-not/
+def is_bst_optimal(node):
+    return is_bst_util(node, -math.inf, math.inf)
+
+
+def is_bst_util(node, min_val, max_val):
+    if node is None:
+        return True
+    if node.val < min_val or node.val > max_val:
+        return False
+    return is_bst_util(node.left, min_val, node.val - 1) \
+        and is_bst_util(node.right, node.val + 1, max_val)
