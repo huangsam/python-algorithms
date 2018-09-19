f4be08e4eee038499e3238790c57ea422ab268a9
Samuel Huang <samhuang91@gmail.com>
2018-09-18 18:30:49 -0700

    Create car_cdr.py

---
+++ online/glassdoor/car_cdr.py
@@ -0,0 +1,16 @@
+def car(pair):
+    def get_first(x, y):
+        return x
+    return pair(get_first)
+
+
+def cdr(pair):
+    def get_last(x, y):
+        return y
+    return pair(get_last)
+
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
