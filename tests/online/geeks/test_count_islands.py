7fb0e00624568e8104885b0381d0063d92fb18ba
Samuel Huang <samhuang91@gmail.com>
2018-09-22 09:52:18 -0700

    Come up with simpler test cases

--- tests/online/geeks/test_count_islands.py
+++ tests/online/geeks/test_count_islands.py
@@ -7,12 +7,25 @@ class TestCountIslands(object):
 
     @pytest.mark.parametrize("grid, answer", [
         ([
-            [1, 1, 0, 0, 0],
-            [0, 1, 0, 0, 1],
-            [1, 0, 0, 1, 1],
-            [0, 0, 0, 0, 0],
-            [1, 0, 1, 0, 1],
-        ], 5),
+            [1, 0, 1],
+            [0, 0, 0],
+            [1, 0, 1],
+        ], 4),
+        ([
+            [1, 0, 1],
+            [1, 0, 1],
+            [1, 0, 1],
+        ], 2),
+        ([
+            [1, 0, 1],
+            [0, 1, 0],
+            [1, 0, 1],
+        ], 1),
+        ([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ], 0),
     ])
     def test_count_islands(self, grid, answer):
         assert count_islands(grid) == answer
