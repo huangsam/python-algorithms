6c182e249c8cdc2fa5503a065de5ea0c9245e158
Samuel Huang <samhuang91@gmail.com>
2018-09-22 06:43:54 -0700

    Implement count_unival solution

---
+++ tests/online/geeks/test_count_unival.py
@@ -0,0 +1,34 @@
+from collection.tree import TreeNode
+from online.geeks.count_unival import count_unival
+
+
+class TestCountUnival(object):
+
+    def test_count_unival_full(self):
+        root = TreeNode(0)
+        root.left = TreeNode(1)
+        root.right = TreeNode(0)
+        root.right.left = TreeNode(1)
+        root.right.right = TreeNode(0)
+        root.right.left.left = TreeNode(1)
+        root.right.left.right = TreeNode(1)
+        assert count_unival(root) == 5
+
+    def test_count_unival_partial_1(self):
+        root = TreeNode(0)
+        root.left = TreeNode(1)
+        root.left.left = TreeNode(1)
+        root.right = TreeNode(0)
+        root.right.left = TreeNode(1)
+        root.right.right = TreeNode(0)
+        root.right.left.left = TreeNode(1)
+        root.right.left.right = TreeNode(1)
+        assert count_unival(root) == 6
+
+    def test_count_unival_partial_2(self):
+        root = TreeNode(5)
+        root.left = TreeNode(5)
+        root.left.left = TreeNode(5)
+        root.right = TreeNode(5)
+        root.right.right = TreeNode(5)
+        assert count_unival(root) == 5
