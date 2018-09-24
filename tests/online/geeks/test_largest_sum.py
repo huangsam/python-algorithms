74cf6111abf32f794a5d7a48bf18a787d91e9b6f
Samuel Huang <samhuang91@gmail.com>
2018-09-24 08:53:35 -0700

    Add more corner cases

--- tests/online/geeks/test_largest_sum.py
+++ tests/online/geeks/test_largest_sum.py
@@ -17,12 +17,14 @@ class TestLargestSum(object):
         ([3, 2, 7, 10], 13),
         ([3, 2, 5, 10, 7], 15),
         ([6, 0, 0, 92, 12], 98),
+        ([-1, 0, -3, 5, 8], 8),
     ])
     def test_largest_sum_non_adjacent(self, given, expected):
         assert largest_sum_non_adjacent(given) == expected
 
     @pytest.mark.parametrize("given, expected", [
         ([], 0),
+        ([1], 1),
         ([-1], 0),
         ([-1, 2], 2),
         ([2, 6, -13, 9, 3, -3, 5, 6], 20),
