79b58da06ae057d06df8e212af344ba9a33364c5
Samuel Huang <samhuang91@gmail.com>
2018-09-23 22:40:15 -0700

    Shorten base case

--- online/geeks/largest_sum.py
+++ online/geeks/largest_sum.py
@@ -1,9 +1,7 @@
 # https://www.geeksforgeeks.org/maximum-sum-such-that-no-two-elements-are-adjacent/
 def largest_sum_non_adjacent(arr):
-    if len(arr) == 0:
-        return 0
-    elif len(arr) < 3:
-        return max(arr)
+    if len(arr) < 3:
+        return 0 if len(arr) == 0 else max(arr)
     result = None
     answers = [0] * len(arr)
     answers[0] = arr[0]
