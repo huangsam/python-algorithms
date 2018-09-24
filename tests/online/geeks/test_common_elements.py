57c680052e959617e6c17736bd96d09fe0256978
Samuel Huang <samhuang91@gmail.com>
2018-09-24 08:57:04 -0700

    Add more corner cases

--- tests/online/geeks/test_common_elements.py
+++ tests/online/geeks/test_common_elements.py
@@ -6,8 +6,10 @@ from online.geeks.common_elements import common_elements
 class TestCommonElements(object):
 
     @pytest.mark.parametrize('a1, a2, a3, expected', [
-        ([1, 3, 5, 7, 9], [1, 5, 9], [3, 5, 7, 9], [5, 9]),
+        ([], [], [], []),
         ([1, 1, 1, 3], [1, 3], [1, 1], [1]),
+        ([1, 3, 5, 7, 9], [1, 5, 9], [3, 5, 7, 9], [5, 9]),
+        ([1, 3, 5], [1, 3, 5], [1, 3, 5, 9], [1, 3, 5]),
     ])
     def test_common_elements(self, a1, a2, a3, expected):
         result = common_elements(a1, a2, a3)
