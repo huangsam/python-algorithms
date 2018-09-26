8f1957a3023d4cce9ff75d39bc2574cd1536d8df
Samuel Huang <samhuang91@gmail.com>
2018-09-26 15:27:52 -0700

    Create autocomplete.py

---
+++ online/geeks/autocomplete.py
@@ -0,0 +1,21 @@
+from collection.trie import Trie
+
+
+# https://www.geeksforgeeks.org/auto-complete-feature-using-trie/
+def autocomplete(s, queries):
+    trie = Trie()
+    for query in queries:
+        trie.insert(query)
+    is_word, node = trie.search(s)
+    if node is None:
+        return []
+    result = []
+    to_visit = [(node, s)]
+    while len(to_visit) > 0:
+        cur_node, cur_str = to_visit.pop(0)
+        if cur_node.is_complete is True:
+            result.append(cur_str)
+            continue
+        for child in cur_node.children.values():
+            to_visit.append((child, cur_str + child.value))
+    return result
