60fd3576a51dcc4eb0116a18a5c21ff09f8199f6
Samuel Huang <samhuang91@gmail.com>
2018-09-26 12:42:25 -0700

    Update test_longest_k.py

--- tests/online/geeks/test_longest_k.py
+++ tests/online/geeks/test_longest_k.py
@@ -5,11 +5,11 @@ from online.geeks.longest_k import longest_k_distinct
 
 class TestLongestK(object):
 
-    @pytest.mark.parametrize("given, k, expected", [
+    @pytest.mark.parametrize("kin, kval, expected", [
         ('abcba', 2, 'bcb'),
         ('ddcbaaa', 3, 'cbaaa'),
         ('ddcbaaa', 2, 'baaa'),
         ('a', 2, ''),
     ])
-    def test_longest_k_distinct(self, given, k, expected):
-        assert longest_k_distinct(given, k, {}) == expected
+    def test_longest_k_distinct(self, kin, kval, expected):
+        assert longest_k_distinct(kin, kval, {}) == expected
