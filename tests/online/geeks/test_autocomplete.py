873d20babb4089b6e6fcab16c8ba11ed8ae889b8
Samuel Huang <samhuang91@gmail.com>
2018-09-26 15:37:17 -0700

    Create test_autocomplete.py

---
+++ tests/online/geeks/test_autocomplete.py
@@ -0,0 +1,15 @@
+import pytest
+
+from online.geeks.autocomplete import autocomplete
+
+
+class TestAutocomplete(object):
+
+    @pytest.mark.parametrize("s, queries, expected", [
+        ('de', ['dog', 'deer', 'deal'], ['deer', 'deal']),
+        ('ab', ['abc', 'abcd', 'aa', 'abbbaba'], ['abc', 'abcd', 'abbbaba']),
+    ])
+    def test_autocomplete(self, s, queries, expected):
+        result = autocomplete(s, queries)
+        for i, j in zip(result, expected):
+            assert i == j
