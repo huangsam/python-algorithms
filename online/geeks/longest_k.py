7039ce523e2ee315057bcd981223af8854c87ec2
Samuel Huang <samhuang91@gmail.com>
2018-09-26 21:34:01 -0700

    Implement longest_k optimally

--- online/geeks/longest_k.py
+++ online/geeks/longest_k.py
@@ -1,4 +1,3 @@
-# https://www.geeksforgeeks.org/find-the-longest-substring-with-k-unique-characters-in-a-given-string/
 def longest_k_distinct(s, k, answers):
     if len(s) == 0:
         return ''
@@ -7,10 +6,9 @@ def longest_k_distinct(s, k, answers):
     if is_valid(s, k) is True:
         answers[s] = s
     else:
-        answers[s] = longest_two(
-            longest_k_distinct(s[1:], k, answers),
-            longest_k_distinct(s[:-1], k, answers)
-        )
+        left = longest_k_distinct(s[1:], k, answers)
+        right = longest_k_distinct(s[:-1], k, answers)
+        answers[s] = left if len(left) > len(right) else right
     return answers[s]
 
 
@@ -19,5 +17,38 @@ def is_valid(s, k):
     return len(unique) == k
 
 
-def longest_two(s1, s2):
-    return s1 if len(s1) > len(s2) else s2
+# https://www.geeksforgeeks.org/find-the-longest-substring-with-k-unique-characters-in-a-given-string/
+def longest_k_distinct_optimal(s, k):
+    counts = {}
+    unique = 0
+    for val in s:
+        if val not in counts:
+            counts[val] = 0
+            unique += 1
+        counts[val] += 1
+    if unique < k:
+        return ''
+    counts = {}
+    start, end = 0, 0
+    result_start = 0
+    result_size = 0
+    for val in s:
+        if val not in counts:
+            counts[val] = 0
+        counts[val] += 1
+        end += 1
+        while is_valid_optimal(counts, k) is False:
+            sval = s[start]
+            counts[sval] -= 1
+            if counts[sval] == 0:
+                del counts[sval]
+            start += 1
+        if (end - start + 1) > result_size:
+            result_size = end - start + 1
+            result_start = start
+    print(s, result_start, result_size)
+    return s[result_start:(result_start + result_size - 1)]
+
+
+def is_valid_optimal(counts, k):
+    return len(counts) <= k
