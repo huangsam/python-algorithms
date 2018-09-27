9c028ddd25cc8b27e7abbab13210ea784e249913
Samuel Huang <samhuang91@gmail.com>
2018-09-26 21:58:17 -0700

    Add corner cases to autocomplete

--- tests/online/geeks/test_autocomplete.py
+++ tests/online/geeks/test_autocomplete.py
@@ -8,6 +8,9 @@ class TestAutocomplete(object):
     @pytest.mark.parametrize("s, queries, expected", [
         ('de', ['dog', 'deer', 'deal'], ['deer', 'deal']),
         ('ab', ['abc', 'abcd', 'aa', 'abbbaba'], ['abc', 'abcd', 'abbbaba']),
+        ('c', ['cat', 'catch', 'catcher'], ['cat', 'catch', 'catcher']),
+        ('b', ['ham', 'clam'], []),
+        ('', ['a', 'ab'], ['a', 'ab']),
     ])
     def test_autocomplete(self, s, queries, expected):
         result = autocomplete(s, queries)
