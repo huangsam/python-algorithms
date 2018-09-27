1d4992eb7dad3a53f348de2cd433ccbd367ad22d
Samuel Huang <samhuang91@gmail.com>
2018-09-26 21:43:24 -0700

    Consolidate routine tests

--- tests/online/geeks/test_longest_k.py
+++ tests/online/geeks/test_longest_k.py
@@ -15,15 +15,6 @@ class TestLongestK(object):
         ('aabbcc', 3, 'aabbcc'),
         ('hatsofftowhoever', 4, 'tsoffto'),
     ])
-    def test_longest_k_distinct(self, kin, kval, expected):
-        assert longest_k_distinct(kin, kval, {}) == expected
-
-    @pytest.mark.parametrize('kin, kval, expected', [
-        ('a', 2, ''),
-        ('aabbcc', 1, 'aa'),
-        ('aabbcc', 2, 'aabb'),
-        ('aabbcc', 3, 'aabbcc'),
-        ('hatsofftowhoever', 4, 'tsoffto'),
-    ])
-    def test_longest_k_distinct_optimal(self, kin, kval, expected):
-        assert longest_k_distinct_optimal(kin, kval) == expected
+    @pytest.mark.parametrize('func', [longest_k_distinct, longest_k_distinct_optimal])
+    def test_longest_k_distinct(self, kin, kval, expected, func):
+        assert func(kin, kval) == expected
