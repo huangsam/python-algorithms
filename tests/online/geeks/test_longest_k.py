597b0d592226e422bd1e1dd30bbd75af3092dd49
Samuel Huang <samhuang91@gmail.com>
2018-09-26 21:34:01 -0700

    Implement longest_k optimally

--- tests/online/geeks/test_longest_k.py
+++ tests/online/geeks/test_longest_k.py
@@ -1,18 +1,29 @@
 import pytest
 
-from online.geeks.longest_k import longest_k_distinct
+from online.geeks.longest_k import (
+    longest_k_distinct,
+    longest_k_distinct_optimal,
+)
 
 
 class TestLongestK(object):
 
-    @pytest.mark.parametrize("kin, kval, expected", [
-        ('abcba', 2, 'bcb'),
-        ('ddcbaaa', 3, 'cbaaa'),
-        ('ddcbaaa', 2, 'baaa'),
+    @pytest.mark.parametrize('kin, kval, expected', [
         ('a', 2, ''),
-        ('aaaaaaa', 2, ''),
+        ('aabbcc', 1, 'aa'),
+        ('aabbcc', 2, 'aabb'),
+        ('aabbcc', 3, 'aabbcc'),
         ('hatsofftowhoever', 4, 'tsoffto'),
-        ('aaabcccd', 2, 'aaab'),
     ])
     def test_longest_k_distinct(self, kin, kval, expected):
         assert longest_k_distinct(kin, kval, {}) == expected
+
+    @pytest.mark.parametrize('kin, kval, expected', [
+        ('a', 2, ''),
+        ('aabbcc', 1, 'aa'),
+        ('aabbcc', 2, 'aabb'),
+        ('aabbcc', 3, 'aabbcc'),
+        ('hatsofftowhoever', 4, 'tsoffto'),
+    ])
+    def test_longest_k_distinct_optimal(self, kin, kval, expected):
+        assert longest_k_distinct_optimal(kin, kval) == expected
