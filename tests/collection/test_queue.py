60d169ac5c1a54e7f17aa9c961fa32440d21c9e6
Samuel Huang <samhuang91@gmail.com>
2018-09-28 09:44:22 -0700

    Add FunkyQueue tests

--- tests/collection/test_queue.py
+++ tests/collection/test_queue.py
@@ -1,4 +1,7 @@
-from collection.queue import Queue
+from collection.queue import (
+    Queue,
+    FunkyQueue,
+)
 
 
 class TestQueue(object):
@@ -16,6 +19,28 @@ class TestQueue(object):
 
     def test_size(self):
         queue = Queue()
+        for i in range(100):
+            queue.push(i)
+        for i in range(20):
+            queue.pop()
+        assert queue.size() == 80
+
+
+class TestFunkyQueue(object):
+
+    def test_push(self):
+        queue = FunkyQueue()
+        queue.push(1)
+        queue.push(2)
+
+    def test_pop(self):
+        queue = FunkyQueue()
+        queue.push(1)
+        queue.push(2)
+        assert queue.pop() == 1
+
+    def test_size(self):
+        queue = FunkyQueue()
         for i in range(100):
             queue.push(i)
         for i in range(20):
