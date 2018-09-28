83b107fabd6a2495dc01cb1b12c29478c9e24d77
Samuel Huang <samhuang91@gmail.com>
2018-09-27 17:29:05 -0700

    Create merge_intervals.py

---
+++ online/geeks/merge_intervals.py
@@ -0,0 +1,21 @@
+# https://www.geeksforgeeks.org/merging-intervals/
+def merge_intervals(intervals):
+    if len(intervals) < 2:
+        return intervals
+    if len(intervals) == 2:
+        return merge_two(intervals[0], intervals[1])
+
+    middle = len(intervals) // 2
+    left = merge_intervals(intervals[:middle])
+    right = merge_intervals(intervals[middle:])
+
+    result = left[:-1] + merge_two(left[-1], right[0]) + right[1:]
+    return result
+
+
+def merge_two(i1, i2):
+    i1_min, i1_max = i1
+    i2_min, i2_max = i2
+    if i1_max < i2_min or i2_max < i1_min:
+        return [i1, i2]
+    return [[min(i1_min, i2_min), max(i1_max, i2_max)]]
