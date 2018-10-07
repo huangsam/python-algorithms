e76a91b78335b1d26fe8c9eaf452c6585a73f9bc
Samuel Huang <samhuang91@gmail.com>
2018-10-07 08:35:21 -0700

    Add dp solutions for factorial

--- basic/factorial.py
+++ basic/factorial.py
@@ -14,3 +14,21 @@ def factorial_stack(n):
     while len(stack) > 0:
         result = result * stack.pop()
     return result
+
+
+def factorial_dp_bottom(n):
+    answers = [1] * max(1, n + 1)
+    for i in range(2, n + 1):
+        answers[i] = i * answers[i - 1]
+    return answers[n]
+
+
+def factorial_dp_top(n):
+    return factorial_dp_top_work(n, {0: 1, 1: 1})
+
+
+def factorial_dp_top_work(n, answers):
+    if n in answers:
+        return answers[n]
+    answers[n] = n * factorial_dp_top_work(n - 1, answers)
+    return answers[n]
