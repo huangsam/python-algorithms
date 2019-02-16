145eddbb7c5020eb43fa1b3b0d10884f256f32bf
Samuel Huang <samhuang91@gmail.com>
2019-02-16 10:14:10 -0800

    Refine test cases for multiply

--- tests/basic/test_multiply.py
+++ tests/basic/test_multiply.py
@@ -5,22 +5,25 @@ from basic.multiply import multiply
 
 class TestMultiply(object):
 
-    def test_multiply_zero(self):
-        assert multiply(0, 2) == 0
-        assert multiply(2, 0) == 0
+    @pytest.mark.parametrize('x,y', [
+        (2, 0),
+        (0, 2),
+        (0, 0),
+    ])
+    def test_multiply_zero(self, x, y):
+        assert multiply(x, y) == 0
 
     def test_multiply_even(self):
-        assert multiply(2, 4) == 8
         assert multiply(3, 6) == 18
-        assert multiply(6, 3) == 18
 
     def test_multiply_odd(self):
-        assert multiply(2, 5) == 10
+        assert multiply(6, 3) == 18
 
-    def test_multiply_neg(self):
+    @pytest.mark.parametrize('x,y', [
+        (-1, 1),
+        (1, -1),
+        (-1, -1),
+    ])
+    def test_multiply_neg(self, x, y):
         with pytest.raises(ValueError):
-            multiply(-1, 5)
-        with pytest.raises(ValueError):
-            multiply(5, -1)
-        with pytest.raises(ValueError):
-            multiply(-1, -1)
+            multiply(x, y)
