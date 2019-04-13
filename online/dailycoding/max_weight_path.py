90dc714c8d84f57c4decf36e16a2943be3cfd039
Samuel Huang <samhuang91@gmail.com>
2019-04-12 18:30:57 -0700

    Use for loops instead of while loops

--- online/dailycoding/max_weight_path.py
+++ online/dailycoding/max_weight_path.py
@@ -1,17 +1,13 @@
 def max_weight_path(triangle):
     height = len(triangle)
-    i = height - 1
     prev_val = None
-    while i > 0:
+    for i in range(height - 1, 0, -1):
         sums = []
         above = triangle[i - 1]
         row = prev_val or triangle[i]
         row_len = len(row)
-        j = 0
-        while j < row_len - 1:
+        for j in range(row_len - 1):
             row_val = max(row[j] + above[j], row[j + 1] + above[j])
             sums.append(row_val)
-            j += 1
         prev_val = sums
-        i -= 1
     return prev_val[0]
