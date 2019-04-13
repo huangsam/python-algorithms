1a947fa8a11c73680cdd6152dc894ede810ca9e5
Samuel Huang <samhuang91@gmail.com>
2019-04-12 18:27:42 -0700

    Implement max_weight_path

---
+++ online/dailycoding/max_weight_path.py
@@ -0,0 +1,21 @@
+def max_weight_path(triangle):
+    height = len(triangle)
+    result = 0
+    i = height - 1
+    prev_val = None
+    while i > 0:
+        sums = []
+        above = triangle[i - 1]
+        if prev_val is None:
+            row = triangle[i]
+        else:
+            row = prev_val
+        row_len = len(row)
+        j = 0
+        while j < row_len - 1:
+            row_val = max(row[j] + above[j], row[j + 1] + above[j])
+            sums.append(row_val)
+            j += 1
+        prev_val = sums
+        i -= 1
+    return prev_val[0]
