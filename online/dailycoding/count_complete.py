184e34a9394db180e4375fd8d788f987dafc92f8
Samuel Huang <samhuang91@gmail.com>
2019-04-12 18:01:07 -0700

    Remove extra cruft in count_complete

--- online/dailycoding/count_complete.py
+++ online/dailycoding/count_complete.py
@@ -5,13 +5,10 @@ def count_complete(root):
     right_c = count_right(root)
     if left_c == right_c:
         return 2 ** left_c - 1
-    else:
-        return 1 + count_complete(root.left) + count_complete(root.right)
+    return 1 + count_complete(root.left) + count_complete(root.right)
 
 
 def count_left(node):
-    if not node:
-        return 0
     height = 0
     while node:
         node = node.left
@@ -20,8 +17,6 @@ def count_left(node):
 
 
 def count_right(node):
-    if not node:
-        return 0
     height = 0
     while node:
         node = node.right
