b0ecaee47e898f9f25d96d8fe3ceffc6de682966
Samuel Huang <samhuang91@gmail.com>
2019-04-12 18:27:42 -0700

    Implement max_weight_path

---
+++ tests/online/dailycoding/test_max_weight_path.py
@@ -0,0 +1,11 @@
+from online.dailycoding.max_weight_path import max_weight_path
+
+
+class TestMaxWeightPath:
+    def test_max_weight_path(self):
+        triangle = [[1], [2, 3], [1, 5, 1]]
+        assert max_weight_path(triangle) == 1 + 3 + 5
+
+    def test_max_weight_corner(self):
+        triangle = [[1], [10, 1], [1, 1, 100]]
+        assert max_weight_path(triangle) == 1 + 1 + 100
