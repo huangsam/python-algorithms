b8bde0e72f60b77a7de13b5717f38ac086b7f184
Samuel Huang <samhuang91@gmail.com>
2019-04-25 23:21:21 -0700

    Add additional test cases

--- online/dailycoding/inorder_optimal.py
+++ online/dailycoding/inorder_optimal.py
@@ -1,10 +1,3 @@
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
-
-
 def rightmost(node):
     prev = None
     cur = node
@@ -33,28 +26,3 @@ def inorder_optimal(root):
             result.append(cur.val)
             cur = cur.right
     return result
-
-
-def main():
-    """
-    Tree example:
-
-            1
-           / \
-          2   3
-         /
-        5
-       / \
-      8   6
-    """
-    r = TreeNode(1)
-    r.left = TreeNode(2)
-    r.left.left = TreeNode(5)
-    r.left.left.left = TreeNode(8)
-    r.left.left.right = TreeNode(6)
-    r.right = TreeNode(3)
-    assert inorder_optimal(r) == [8, 5, 6, 2, 1, 3]
-
-
-if __name__ == '__main__':
-    main()
