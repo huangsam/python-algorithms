5b396f39bf72d1fc4f7709483baa8c739aec174a
Samuel Huang <samhuang91@gmail.com>
2019-04-25 23:50:43 -0700

    Make module imports look shorter

--- tests/basic/test_factorial.py
+++ tests/basic/test_factorial.py
@@ -1,18 +1,18 @@
 import pytest
 
-from algorithms.basic.factorial import (
-    factorial_recursive,
-    factorial_stack,
-    factorial_dp_bottom,
-    factorial_dp_top,
-)
+from algorithms.basic import factorial as fact
 
 
 @pytest.mark.dynamic
 class TestFactorial:
     @pytest.mark.parametrize(
         "func",
-        [factorial_recursive, factorial_stack, factorial_dp_bottom, factorial_dp_top],
+        [
+            fact.factorial_recursive,
+            fact.factorial_stack,
+            fact.factorial_dp_bottom,
+            fact.factorial_dp_top,
+        ],
     )
     @pytest.mark.parametrize("i,o", [(0, 1), (1, 1), (2, 2), (3, 6), (4, 24), (5, 120)])
     def test_factorial(self, func, i, o):
--- tests/tree/test_postorder.py
+++ tests/tree/test_postorder.py
@@ -1,16 +1,16 @@
 import pytest
 
-from algorithms.tree.postorder import postorder_iterative, postorder_recursive
+from algorithms.tree import postorder as post_
 
 
 @pytest.mark.tree
 class TestPostorder:
     def test_postorder_iterative(self, simple_tree):
-        result = postorder_iterative(simple_tree)
+        result = post_.postorder_iterative(simple_tree)
         assert (len(result)) == 6
         assert "3,5,6,4,2,1" == ",".join(map(str, result))
 
     def test_postorder_recursive(self, simple_tree):
-        result = postorder_recursive(simple_tree)
+        result = post_.postorder_recursive(simple_tree)
         assert (len(result)) == 6
         assert "3,5,6,4,2,1" == ",".join(map(str, result))
