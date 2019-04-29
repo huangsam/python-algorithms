d986733daa5f81d5dbec5c27c998681a4f13f112
Samuel Huang <samhuang91@gmail.com>
2019-04-29 10:07:35 -0700

    Implement rod_cutting

---
+++ tests/dynamic/test_rod_cutting.py
@@ -0,0 +1,18 @@
+import pytest
+
+from algorithms.dynamic import rod_cutting as rods
+
+
+class TestRodCutting:
+    @pytest.mark.parametrize("func", [rods.rod_cutting_dp, rods.rod_cutting_rec])
+    def test_rod_cutting(self, func):
+        prices = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+        n = 4
+        assert func(n, prices) == 10
+        assert func(1, prices) == prices[0]
+        assert func(0, prices) == 0
+
+    def test_rod_cutting_equal(self):
+        prices = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+        for n in range(len(prices)):
+            assert rods.rod_cutting_dp(n, prices) == rods.rod_cutting_rec(n, prices)
