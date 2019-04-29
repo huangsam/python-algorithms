e9d59c36c8e4021c2c6c968208a2bcdf2c7b27a0
Samuel Huang <samhuang91@gmail.com>
2019-04-29 10:07:35 -0700

    Implement rod_cutting

---
+++ algorithms/dynamic/rod_cutting.py
@@ -0,0 +1,22 @@
+def rod_cutting_rec(n, prices):
+    if n == 0:
+        return 0
+    if n == 1:
+        return prices[0]
+    result = -1
+    for i in range(1, n + 1):
+        cut = prices[i - 1]
+        remainder = rod_cutting_rec(n - i, prices)
+        result = max(cut + remainder, result)
+    return result
+
+
+def rod_cutting_dp(n, prices):
+    revenue = [0] * max(2, n + 1)
+    revenue[1] = prices[0]
+    if n < 2:
+        return revenue[n]
+    for i in range(2, n + 1):
+        for j in range(1, i + 1):
+            revenue[i] = max(prices[j - 1] + revenue[i - j], revenue[i])
+    return revenue[n]
