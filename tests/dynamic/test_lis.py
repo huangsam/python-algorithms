a38a49c5bc395f3ad1a87c562a5d2fe5b5ab04e7
Samuel Huang <samhuang91@gmail.com>
2019-04-30 23:09:40 -0700

    Implement lis dynamically

---
+++ tests/dynamic/test_lis.py
@@ -0,0 +1,20 @@
+import pytest
+
+from algorithms.dynamic.lis import lis
+
+
+@pytest.mark.dynamic
+class TestLis:
+    @pytest.mark.parametrize("n", [1, 5, 100])
+    def test_lis_always(self, n):
+        a = [_ for _ in range(n)]
+        assert lis(a) == n
+
+    @pytest.mark.parametrize("n", [1, 5, 100])
+    def test_lis_never(self, n):
+        a = [_ for _ in range(n - 1, -1, -1)]
+        assert lis(a) == 1
+
+    def test_lis_mixed(self):
+        a = [1, 12, 7, 0, 23, 11, 52, 31, 61, 69, 70, 2]
+        assert lis(a) == 7
