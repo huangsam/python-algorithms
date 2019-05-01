424762ec6ea10a899e4e536a7d95bbe8a87fc426
Samuel Huang <samhuang91@gmail.com>
2019-05-01 10:26:03 -0700

    Implement lcs_dp dynamically

--- algorithms/dynamic/lcs.py
+++ algorithms/dynamic/lcs.py
@@ -1,22 +1,30 @@
 def lcs_rec(astr, bstr):
-    return lcs_wh(astr, 0, bstr, 0)
+    return lcs_wh(astr, len(astr) - 1, bstr, len(bstr) - 1)
 
 
 def lcs_wh(astr, aind, bstr, bind):
-    if aind == len(astr) or bind == len(bstr):
+    if aind < 0 or bind < 0:
         return 0
 
     if astr[aind] == bstr[bind]:
-        match = 1 + lcs_wh(astr, aind + 1, bstr, bind + 1)
+        match = 1 + lcs_wh(astr, aind - 1, bstr, bind - 1)
         return match
 
-    alcs = lcs_wh(astr, aind + 1, bstr, bind)
-    blcs = lcs_wh(astr, aind, bstr, bind + 1)
+    alcs = lcs_wh(astr, aind - 1, bstr, bind)
+    blcs = lcs_wh(astr, aind, bstr, bind - 1)
     return max(alcs, blcs)
 
 
 # https://algorithms.tutorialhorizon.com//dynamic-programming-longest-common-subsequence/
 def lcs_dp(astr, bstr):
     alen, blen = len(astr), len(bstr)
-    matches = [[0] * alen for _ in blen]
+    matches = [[0] * (blen + 1) for _ in range(alen + 1)]
+    for i in range(1, alen + 1):
+        for j in range(1, blen + 1):
+            if astr[i - 1] == bstr[j - 1]:
+                matches[i][j] = 1 + matches[i - 1][j - 1]
+            else:
+                alcs = matches[i - 1][j]
+                blcs = matches[i][j - 1]
+                matches[i][j] = max(alcs, blcs)
     return matches[-1][-1]
