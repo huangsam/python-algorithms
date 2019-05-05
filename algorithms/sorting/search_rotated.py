b59e120dff7deeeb7922243c5d2d9ffaa4e21cc1
Samuel Huang <samhuang91@gmail.com>
2019-05-05 09:02:45 -0500

    Implement search_min

--- algorithms/sorting/search_rotated.py
+++ algorithms/sorting/search_rotated.py
@@ -16,3 +16,23 @@ def search_rotated(nums, target):
             else:
                 lo, hi = lo, mid - 1
     return False
+
+
+def search_min(nums):
+    return search_min_wh(nums, 0, len(nums) - 1)
+
+
+def search_min_wh(nums, lo, hi):
+    mid = (lo + hi) // 2
+
+    if lo == hi:
+        return nums[lo]
+
+    if nums[lo] <= nums[mid] <= nums[hi]:
+        return nums[lo]
+
+    if nums[lo] > nums[mid]:
+        return search_min_wh(nums, lo + 1, mid)
+
+    if nums[mid] > nums[hi]:
+        return search_min_wh(nums, mid + 1, hi)
