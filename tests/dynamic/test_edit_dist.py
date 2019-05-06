47288072c5c7283de0fbfbe84d1237f1f8dfaafd
Samuel Huang <samhuang91@gmail.com>
2019-05-06 11:00:21 -0700

    Consolidate test cases for edit_dist

--- tests/dynamic/test_edit_dist.py
+++ tests/dynamic/test_edit_dist.py
@@ -6,27 +6,18 @@ import algorithms.dynamic.edit_dist as edit
 @pytest.mark.string
 @pytest.mark.dynamic
 class TestEditDist:
-    def test_edit_dist_same(self):
-        assert edit.edit_dist_rec("abcd", "abcd") == 0
-
-    def test_edit_dist_insert(self):
-        assert edit.edit_dist_rec("a", "abcd") == 3
-
-    def test_edit_dist_remove(self):
-        assert edit.edit_dist_rec("abcd", "a") == 3
-
-    def test_edit_dist_replace(self):
-        assert edit.edit_dist_rec("abcd", "abce") == 1
-
     @pytest.mark.parametrize(
         "a, b, o",
         [
-            ("achoo", "aching", 3),
-            ("geek", "gkee", 2),
+            ("abcd", "abcd", 0),
+            ("a", "abc", 2),
+            ("abc", "a", 2),
             ("cat", "cut", 1),
+            ("geek", "gkee", 2),
+            ("achoo", "aching", 3),
             ("sunday", "saturday", 3),
         ],
     )
     @pytest.mark.parametrize("f", [edit.edit_dist_rec, edit.edit_dist_dp])
-    def test_edit_dist_all(self, f, a, b, o):
+    def test_edit_dist(self, f, a, b, o):
         assert f(a, b) == o
