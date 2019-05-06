9fb9253f2e7a9d57c5f1cc9e35d3499e00f64332
Samuel Huang <samhuang91@gmail.com>
2019-05-06 12:43:13 -0700

    Consolidate num_to_word test cases

--- tests/online/geeks/test_num_to_word.py
+++ tests/online/geeks/test_num_to_word.py
@@ -5,27 +5,25 @@ import algorithms.online.geeks.num_to_word as word
 
 @pytest.mark.string
 class TestNumToWords:
-    def test_num_to_word_single(self):
-        assert word.num_to_word(0) == "zero"
-        assert word.num_to_word(1) == "one"
-        assert word.num_to_word(9) == "nine"
-
-    def test_num_to_word_double(self):
-        assert word.num_to_word(10) == "ten"
-        assert word.num_to_word(11) == "eleven"
-        assert word.num_to_word(19) == "nineteen"
-
-    def test_num_to_word_hundred(self):
-        assert word.num_to_word(100) == "one hundred"
-        assert word.num_to_word(200) == "two hundred"
-        assert word.num_to_word(202) == "two hundred two"
-        assert word.num_to_word(999) == "nine hundred ninety nine"
-
-    def test_num_to_word_thousands(self):
-        assert word.num_to_word(1234) == "one thousand two hundred thirty four"
-        assert word.num_to_word(10101) == "ten thousand one hundred one"
-        assert word.num_to_word(101010) == "one hundred one thousand ten"
-        assert word.num_to_word(10000001) == "ten million one"
-        assert word.num_to_word(12000034) == "twelve million thirty four"
-        assert word.num_to_word(10 ** 9 + 1) == "one billion one"
-        assert word.num_to_word(10 ** 12 + 1) == "one trillion one"
+    @pytest.mark.parametrize(
+        "a, o",
+        [
+            (0, "zero"),
+            (9, "nine"),
+            (10, "ten"),
+            (19, "nineteen"),
+            (100, "one hundred"),
+            (200, "two hundred"),
+            (202, "two hundred two"),
+            (999, "nine hundred ninety nine"),
+            (1234, "one thousand two hundred thirty four"),
+            (10101, "ten thousand one hundred one"),
+            (101010, "one hundred one thousand ten"),
+            (10000001, "ten million one"),
+            (12000034, "twelve million thirty four"),
+            (10 ** 9 + 1, "one billion one"),
+            (10 ** 12 + 1, "one trillion one"),
+        ],
+    )
+    def test_num_to_word(self, a, o):
+        assert word.num_to_word(a) == o
