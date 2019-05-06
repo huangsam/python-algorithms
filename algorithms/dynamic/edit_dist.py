01ef0a3970bc1e4cf46e90e4fc3eab01bad97a75
Samuel Huang <samhuang91@gmail.com>
2019-05-06 10:56:58 -0700

    Implement edit_dist

--- algorithms/dynamic/edit_dist.py
+++ algorithms/dynamic/edit_dist.py
@@ -17,13 +17,16 @@ def edit_dist_wh(s1, i1, s2, i2):
 
 
 def edit_dist_dp(s1, s2):
-    dist = [[0] * len(s2) for _ in range(len(s1))]
-    for i in range(len(s1)):
-        for j in range(len(s2)):
+    l1, l2 = len(s1), len(s2)
+    dist = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+    for i in range(l2 + 1):
+        dist[0][i] = i
+    for i in range(l1 + 1):
+        dist[i][0] = i
+    for i in range(1, l1 + 1):
+        for j in range(1, l2 + 1):
             if s1[i - 1] == s2[j - 1]:
                 dist[i][j] = dist[i - 1][j - 1]
             else:
-                dist[i][j] = min(
-                    1 + dist[i - 1][j], 1 + dist[i][j - 1], 1 + dist[i - 1][j - 1]
-                )
+                dist[i][j] = 1 + min(dist[i - 1][j], dist[i][j - 1], dist[i - 1][j - 1])
     return dist[-1][-1]
--- tests/dynamic/test_edit_dist.py
+++ tests/dynamic/test_edit_dist.py
@@ -18,6 +18,15 @@ class TestEditDist:
     def test_edit_dist_replace(self):
         assert edit.edit_dist_rec("abcd", "abce") == 1
 
+    @pytest.mark.parametrize(
+        "a, b, o",
+        [
+            ("achoo", "aching", 3),
+            ("geek", "gkee", 2),
+            ("cat", "cut", 1),
+            ("sunday", "saturday", 3),
+        ],
+    )
     @pytest.mark.parametrize("f", [edit.edit_dist_rec, edit.edit_dist_dp])
-    def test_edit_dist_all(self, f):
-        assert f("sleepy", "sleeeep") == 3
+    def test_edit_dist_all(self, f, a, b, o):
+        assert f(a, b) == o
