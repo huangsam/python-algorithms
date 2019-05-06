f2d92bd804330f7bc1fed53a1d45b5f3b1addab6
Samuel Huang <samhuang91@gmail.com>
2019-05-06 09:13:01 -0700

    Implement dutch_flag

---
+++ algorithms/online/geeks/dutch_flag.py
@@ -0,0 +1,17 @@
+# https://www.geeksforgeeks.org/sort-an-array-of-0s-1s-and-2s/
+def dutch_flag(arr):
+    result = [*arr]
+    lo = 0
+    mid = 0
+    hi = len(result) - 1
+    while mid <= hi:
+        if result[mid] == 0:
+            result[lo], result[mid] = result[mid], result[lo]
+            lo += 1
+            mid += 1
+        elif result[mid] == 1:
+            mid += 1
+        else:
+            result[mid], result[hi] = result[hi], result[mid]
+            hi -= 1
+    return result
---
+++ tests/online/geeks/test_dutch_flag.py
@@ -0,0 +1,29 @@
+import pytest
+
+import algorithms.online.geeks.dutch_flag as dutch
+
+
+@pytest.mark.array
+class TestDutchFlag:
+    @staticmethod
+    def verify(arr):
+        i = 0
+        for j in (0, 1, 2):
+            while i < len(arr) and arr[i] == j:
+                i += 1
+        assert i == len(arr)
+
+    def test_dutch_flag_sample(self):
+        arr = [0, 1, 2, 0, 1, 2]
+        result = dutch.dutch_flag(arr)
+        self.verify(result)
+
+    def test_dutch_flag_done(self):
+        arr = [0, 0, 1, 1, 2, 2]
+        result = dutch.dutch_flag(arr)
+        self.verify(result)
+
+    def test_dutch_flag_all_2s(self):
+        arr = [2, 2, 2, 2, 2, 2]
+        result = dutch.dutch_flag(arr)
+        self.verify(result)
