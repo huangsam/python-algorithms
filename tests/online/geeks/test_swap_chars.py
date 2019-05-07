aecda8db77c85c2f095e6f38c0204dd4d17e4e7b
Samuel Huang <samhuang91@gmail.com>
2019-05-06 22:23:46 -0700

    Refine tests and swap_chars impl

--- tests/online/geeks/test_swap_chars.py
+++ tests/online/geeks/test_swap_chars.py
@@ -5,14 +5,20 @@ import algorithms.online.geeks.swap_chars as swap
 
 @pytest.mark.string
 class TestSwapChars:
-    def test_swap_chars_bad_length(self):
-        assert swap.swap_chars("a", "ab") is False
-
-    def test_swap_chars_same(self):
-        assert swap.swap_chars("abcd", "abcd") is True
-
-    def test_swap_chars_diff_can(self):
-        assert swap.swap_chars("cdab", "abcd") is True
-
-    def test_swap_chars_diff_cannot(self):
-        assert swap.swap_chars("cdab", "dcba") is False
+    @pytest.mark.parametrize(
+        "a, b, o",
+        [
+            ("", "", True),
+            ("a", "ab", False),
+            ("a", "a", True),
+            ("aa", "aa", True),
+            ("aaaa", "aaaa", True),
+            ("abcd", "abcd", True),
+            ("abc", "cba", True),
+            ("cdab", "abcd", True),
+            ("ab", "ba", False),
+            ("cdab", "dcba", False),
+        ],
+    )
+    def test_swap_chars_bad_length(self, a, b, o):
+        assert swap.swap_chars(a, b) is o
