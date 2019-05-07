53b0ba84a7ade6ccfde7f12081e183cbef76067f
Samuel Huang <samhuang91@gmail.com>
2019-05-06 22:23:46 -0700

    Refine tests and swap_chars impl

--- algorithms/online/geeks/swap_chars.py
+++ algorithms/online/geeks/swap_chars.py
@@ -8,13 +8,13 @@ def swap_new(s1, a, b):
 def swap_chars(s1, s2):
     if len(s1) != len(s2):
         return False
-    if s1 == s2:
-        return True
     i = 0
-    while i < len(s1) and s1[i] == s2[i]:
+    while i < len(s1):
+        while i < len(s1) and s1[i] == s2[i]:
+            i += 1
+        if i + 2 < len(s1):
+            if s1[i + 2] != s2[i]:
+                return False
+            s1 = swap_new(s1, i, i + 2)
         i += 1
-    if i + 2 < len(s1):
-        if s1[i + 2] == s2[i]:
-            s1_new = swap_new(s1, i, i + 2)
-            return swap_chars(s1_new[1:], s2[1:])
-    return False
+    return s1 == s2
