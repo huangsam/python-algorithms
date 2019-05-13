7b101ee7209ce28462325d8ece51eef1647ed972
Samuel Huang <shuang@paloaltonetworks.com>
2019-05-13 12:26:33 -0700

    Revise rval of bsearch

--- tests/basic/test_bsearch.py
+++ tests/basic/test_bsearch.py
@@ -10,8 +10,8 @@ class TestBinarySearch:
     )
     def test_bsearch_good(self, func):
         arr = [0, 2, 4, 6, 8, 10]
-        for v in arr:
-            assert func(arr, v) is True
+        for ix, v in enumerate(arr):
+            assert func(arr, v) == ix
 
     @pytest.mark.parametrize(
         "func", [bsearch.bsearch_iterative, bsearch.bsearch_recursive]
@@ -19,4 +19,4 @@ class TestBinarySearch:
     def test_bsearch_bad(self, func):
         arr = [0, 2, 4, 6, 8, 10]
         for v in arr:
-            assert func(arr, v - 1) is False
+            assert func(arr, v - 1) == -1
