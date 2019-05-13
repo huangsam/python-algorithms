05cf6d1a91a8d185b898eba7e5620561de0e5fb9
Samuel Huang <samhuang91@gmail.com>
2019-05-13 09:57:28 -0700

    Modularize h_index logic

--- algorithms/online/dailycoding/h_index.py
+++ algorithms/online/dailycoding/h_index.py
@@ -1,10 +1,14 @@
+def is_valid(papers, target):
+    valid = 0
+    for paper in papers:
+        if paper >= target:
+            valid += 1
+    return valid >= target
+
+
 def h_index(papers):
     max_score = min(len(papers), max(papers))
     while max_score >= 0:
-        valid = 0
-        for paper in papers:
-            if paper >= max_score:
-                valid += 1
-        if valid >= max_score:
+        if is_valid(papers, max_score):
             return max_score
         max_score -= 1
