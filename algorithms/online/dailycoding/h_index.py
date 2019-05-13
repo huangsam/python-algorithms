4159ed3060a2ad25328365cf211c200d3b5b7f65
Samuel Huang <samhuang91@gmail.com>
2019-05-13 09:14:03 -0700

    Implement h_index naive

---
+++ algorithms/online/dailycoding/h_index.py
@@ -0,0 +1,10 @@
+def h_index(papers):
+    max_score = max(papers)
+    while max_score >= 0:
+        valid = 0
+        for paper in papers:
+            if paper >= max_score:
+                valid += 1
+        if valid >= max_score:
+            return valid
+        max_score -= 1
