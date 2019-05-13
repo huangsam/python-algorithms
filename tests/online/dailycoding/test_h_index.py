e7dae178aaf7871a5955cf719c0afd7c2ee06887
Samuel Huang <samhuang91@gmail.com>
2019-05-13 09:17:50 -0700

    Resolve edge case with h_index

--- tests/online/dailycoding/test_h_index.py
+++ tests/online/dailycoding/test_h_index.py
@@ -5,11 +5,11 @@ class TestHIndex:
     def test_h_index_sample(self):
         assert hind.h_index([4, 3, 0, 1, 5]) == 3
 
-    def test_h_index_single(self):
-        assert hind.h_index([9]) == 1
-
     def test_h_index_zero(self):
-        assert hind.h_index([0]) == 0
+        assert hind.h_index([0, 0, 0, 0, 0]) == 0
+
+    def test_h_index_one(self):
+        assert hind.h_index([1, 1, 1, 1, 1]) == 1
 
     def test_h_index_same(self):
         for i in range(1, 10):
