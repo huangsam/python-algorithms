0539be94edc16b2c5af8623224f019036b382a46
Samuel Huang <samhuang91@gmail.com>
2019-05-13 22:51:54 -0700

    Fixup h_index test cases better

--- tests/online/dailycoding/test_h_index.py
+++ tests/online/dailycoding/test_h_index.py
@@ -19,7 +19,7 @@ class TestHIndex:
     def test_h_index_sample(self, i, o):
         assert hind.h_index(i) == o
 
-    def test_h_index_same(self):
-        for i in range(1, 10):
-            papers = [i for _ in range(i)]
-            assert hind.h_index(papers) == i
+    @pytest.mark.parametrize("i", [_ for _ in range(1, 13)])
+    def test_h_index_same(self, i):
+        papers = [i for _ in range(i)]
+        assert hind.h_index(papers) == i
