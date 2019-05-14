a58b9f9abe8e7c69e60db7668b9064c86abea3ef
Samuel Huang <samhuang91@gmail.com>
2019-05-14 10:02:51 -0700

    Add extra symmetry test case

--- tests/online/codesignal/test_isTreeSymmetric.py
+++ tests/online/codesignal/test_isTreeSymmetric.py
@@ -3,7 +3,7 @@ from algorithms.collection.tree import TreeNode
 
 
 class TestIsTreeSymmetric:
-    def test_symmetric(self):
+    def test_symmetric_good(self):
         root = TreeNode(1)
         root.left = TreeNode(2)
         root.left.left = TreeNode(3)
@@ -12,3 +12,11 @@ class TestIsTreeSymmetric:
         root.right.left = TreeNode(4)
         root.right.right = TreeNode(3)
         assert symmetry.isTreeSymmetric(root)
+
+    def test_symmetric_bad(self):
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.left.right = TreeNode(3)
+        root.right = TreeNode(2)
+        root.right.right = TreeNode(3)
+        assert not symmetry.isTreeSymmetric(root)
