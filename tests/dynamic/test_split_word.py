d5582b1fe4b839a2fefd03907c286e604f85a990
Samuel Huang <samhuang91@gmail.com>
2019-05-15 16:55:23 -0700

    Add test cases for split_word

--- tests/dynamic/test_split_word.py
+++ tests/dynamic/test_split_word.py
@@ -6,14 +6,29 @@ import algorithms.dynamic.split_word as word
 @pytest.mark.string
 @pytest.mark.dynamic
 class TestSplitWord:
-    book = {"I", "have", "Jain", "Sumit", "am", "this", "dog"}
+    book = {
+        "am",
+        "book",
+        "cool",
+        "dog",
+        "face",
+        "facebook",
+        "have",
+        "I",
+        "is",
+        "Jain",
+        "Sumit",
+        "this",
+    }
 
-    def test_split_word_good(self):
+    @pytest.mark.parametrize("i", ["IamSumit", "facebook", "thisdogiscool"])
+    def test_split_word_good(self, i):
         cache = {}
-        result = word.split_word("IamSumit", self.book, cache)
+        result = word.split_word(i, self.book, cache)
         assert result is True
 
-    def test_split_word_bad(self):
+    @pytest.mark.parametrize("i", ["thisisadog", "face", "book"])
+    def test_split_word_bad(self, i):
         cache = {}
-        result = word.split_word("thisisadog", self.book, cache)
+        result = word.split_word(i, self.book, cache)
         assert result is False
