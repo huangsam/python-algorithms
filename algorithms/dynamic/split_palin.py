f7cf32080201b399b19ed05cb62c7e8155b5c743
Samuel Huang <samhuang91@gmail.com>
2019-05-15 16:44:07 -0700

    Optimize split_palin

--- algorithms/dynamic/split_palin.py
+++ algorithms/dynamic/split_palin.py
@@ -4,14 +4,15 @@ def is_palin(seq):
 
 # https://algorithms.tutorialhorizon.com//dynamic-programming-split-the-string-into-minimum-number-of-palindromes/
 def split_palin(seq, cache):
+    if seq in cache:
+        return cache[seq]
     if is_palin(seq):
         cache[seq] = 0
         return cache[seq]
     result = 2 ** 32 - 1
     for i in range(1, len(seq)):
         lseq, rseq = seq[:i], seq[i:]
-        lval = cache.get(lseq, split_palin(lseq, cache))
-        rval = cache.get(rseq, split_palin(rseq, cache))
+        lval, rval = split_palin(lseq, cache), split_palin(rseq, cache)
         result = min(result, 1 + lval + rval)
     cache[seq] = result
     return result
