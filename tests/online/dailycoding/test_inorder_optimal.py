28d94cbe50b893b1554d231c18fd23aca8f7083b
Samuel Huang <samhuang91@gmail.com>
2019-05-15 18:41:52 -0700

    Add inorder_optimal test case

--- tests/online/dailycoding/test_inorder_optimal.py
+++ tests/online/dailycoding/test_inorder_optimal.py
@@ -34,3 +34,18 @@ class TestInorderOptimal:
         r.left.left.right = TreeNode(6)
         r.right = TreeNode(3)
         assert [n for n in inorder_optimal(r)] == [8, 5, 6, 2, 1, 3]
+
+    def test_inorder_optimal_symmetric(self):
+        """
+                1
+               / \
+              2   2
+             /     \
+            3       3
+        """
+        r = TreeNode(1)
+        r.left = TreeNode(2)
+        r.left.left = TreeNode(3)
+        r.right = TreeNode(2)
+        r.right.right = TreeNode(3)
+        assert [n for n in inorder_optimal(r)] == [3, 2, 1, 2, 3]
