8ce4b19f574b788d0a63d6d75a2603721db1983b
Samuel Huang <samhuang91@gmail.com>
2019-05-17 00:13:53 -0700

    Implement brute force split_sum

--- algorithms/dynamic/split_sum.py
+++ algorithms/dynamic/split_sum.py
@@ -1,9 +1,5 @@
 # https://stackoverflow.com/questions/39673898/divide-array-into-k-contiguos-partitions-such-that-sum-of-maximum-partition-is-m
 def split_sum(arr, k):
-    return split_sum_wh(arr, k)
-
-
-def split_sum_wh(arr, k):
     if k == 1:
         return sum(arr)
     if k == len(arr):
@@ -13,7 +9,7 @@ def split_sum_wh(arr, k):
     i = 1
     while i < len(arr):
         lval = sum(arr[start:i])
-        rval = split_sum_wh(arr[i:end], k - 1)
+        rval = split_sum(arr[i:end], k - 1)
         result = min(result, max(lval, rval))
         i += 1
     return result
