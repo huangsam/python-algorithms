d5dbe467b4ca1a46d1a2871f0572b8ed611ec4df
Samuel Huang <samhuang91@gmail.com>
2019-05-17 08:18:21 -0700

    Add extra test cases for split_sum

--- tests/dynamic/test_split_sum.py
+++ tests/dynamic/test_split_sum.py
@@ -16,3 +16,12 @@ class TestSplitSum:
 
     def test_split_sum_sample(self):
         assert split.split_sum([5, 1, 2, 7, 3, 4], 3) == 8
+
+    def test_split_sum_painter_three(self):
+        assert split.split_sum([1] * 6, 3) == 2
+
+    def test_split_sum_painter_five(self):
+        assert split.split_sum([1] * 9, 5) == 2
+
+    def test_split_sum_painter_equal(self):
+        assert split.split_sum([1] * 12, 12) == 1
