afe07d4da3b87e19ff334a43066219f77f2ee37c
Samuel Huang <samhuang91@gmail.com>
2019-05-20 23:20:05 -0700

    Implement can_chain

---
+++ tests/online/dailycoding/test_chainable.py
@@ -0,0 +1,20 @@
+import pytest
+
+from algorithms.online.dailycoding import chainable
+
+
+@pytest.mark.string
+class TestCanChain:
+    @pytest.mark.parametrize(
+        "i, o",
+        [
+            (["chair", "height", "racket", "touch", "tunic"], True),
+            (["chair", "racket", "tunic"], True),
+            (["tulip", "pat"], True),
+            (["chair", "racket"], False),
+            (["chair", "racket", "touch"], False),
+            (["chair", "racket", "touch", "hairy"], False),
+        ],
+    )
+    def test_can_chain(self, i, o):
+        assert chainable.can_chain(i) == o
