45e2448d7599a42d0995aa3d7914984181612d55
Samuel Huang <samhuang91@gmail.com>
2019-05-22 08:06:28 -0700

    Add more phone tests

--- tests/basic/test_phones.py
+++ tests/basic/test_phones.py
@@ -6,12 +6,30 @@ from algorithms.basic import phones
 @pytest.mark.math
 @pytest.mark.string
 class TestPhoneWords:
-    def test_get_phone_words_rec(self):
-        res = phones.get_phone_words_rec()
+    def test_get_phone_words_rec_all(self):
+        res = phones.get_phone_words_rec("23456789")
         assert isinstance(res, list)
         assert len(res) == 3 ** 8
 
-    def test_get_phone_words_stk(self):
-        res = phones.get_phone_words_stk()
+    def test_get_phone_words_stk_all(self):
+        res = phones.get_phone_words_stk("23456789")
         assert isinstance(res, list)
         assert len(res) == 3 ** 8
+
+    def test_get_phone_words_rec_one(self):
+        res = phones.get_phone_words_rec("2")
+        assert isinstance(res, list)
+        assert len(res) == 3
+
+    def test_get_phone_words_stk_one(self):
+        res = phones.get_phone_words_stk("2")
+        assert isinstance(res, list)
+        assert len(res) == 3
+
+    def test_get_phone_words_rec_err(self):
+        with pytest.raises(ValueError):
+            phones.get_phone_words_rec("")
+
+    def test_get_phone_words_stk_err(self):
+        with pytest.raises(ValueError):
+            phones.get_phone_words_stk("")
