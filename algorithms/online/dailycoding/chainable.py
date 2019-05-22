9853209083060b159417b889b83c10a64402452b
Samuel Huang <samhuang91@gmail.com>
2019-05-22 08:13:51 -0700

    Expand varname for clarity

--- algorithms/online/dailycoding/chainable.py
+++ algorithms/online/dailycoding/chainable.py
@@ -6,25 +6,25 @@ def can_chain(words):
     - https://www.geeksforgeeks.org/given-array-strings-find-strings-can-chained-form-circle/
     - https://www.geeksforgeeks.org/find-array-strings-can-chained-form-circle-set-2/
     """
-    fmap = {}
+    first_map = {}
     for word in words:
         ch = word[0]
-        if ch not in fmap:
-            fmap[ch] = set()
-        fmap[ch].add(word)
-    return can_chain_wh(words[0], words, fmap, set())
+        if ch not in first_map:
+            first_map[ch] = set()
+        first_map[ch].add(word)
+    return can_chain_wh(words[0], words, first_map, set())
 
 
-def can_chain_wh(cur_word, words, fmap, seen):
+def can_chain_wh(cur_word, words, first_map, seen):
     if len(words) == len(seen):
         return True
     if cur_word in seen:
         return False
     seen.add(cur_word)
     lch = cur_word[-1]
-    if lch not in fmap:
+    if lch not in first_map:
         return False
-    for next_word in fmap[lch]:
-        if can_chain_wh(next_word, words, fmap, seen):
+    for next_word in first_map[lch]:
+        if can_chain_wh(next_word, words, first_map, seen):
             return True
     return False
