4a37e0a2e97496359d444544e788fb05358dd6fc
Samuel Huang <samhuang91@gmail.com>
2019-05-22 08:10:12 -0700

    Parametrize test cases

--- tests/basic/test_phones.py
+++ tests/basic/test_phones.py
@@ -6,25 +6,17 @@ from algorithms.basic import phones
 @pytest.mark.math
 @pytest.mark.string
 class TestPhoneWords:
-    def test_get_phone_words_rec_all(self):
-        res = phones.get_phone_words_rec("23456789")
+    @pytest.mark.parametrize("i", ["23456789", "3257", "3"])
+    def test_get_phone_words_rec(self, i):
+        res = phones.get_phone_words_rec(i)
         assert isinstance(res, list)
-        assert len(res) == 3 ** 8
+        assert len(res) == 3 ** len(i)
 
-    def test_get_phone_words_stk_all(self):
-        res = phones.get_phone_words_stk("23456789")
+    @pytest.mark.parametrize("i", ["23456789", "3257", "3"])
+    def test_get_phone_words_stk(self, i):
+        res = phones.get_phone_words_stk(i)
         assert isinstance(res, list)
-        assert len(res) == 3 ** 8
-
-    def test_get_phone_words_rec_one(self):
-        res = phones.get_phone_words_rec("2")
-        assert isinstance(res, list)
-        assert len(res) == 3
-
-    def test_get_phone_words_stk_one(self):
-        res = phones.get_phone_words_stk("2")
-        assert isinstance(res, list)
-        assert len(res) == 3
+        assert len(res) == 3 ** len(i)
 
     def test_get_phone_words_rec_err(self):
         with pytest.raises(ValueError):
