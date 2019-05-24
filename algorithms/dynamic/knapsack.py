086a6690efc138113d0f6db8073c0c75a906e93e
Samuel Huang <samhuang91@gmail.com>
2019-05-24 11:51:44 -0700

    Series of small tweaks and bug fixes
    - Implement knapsack recursively
    - Rename answers matrix to total
    - Add test cases for knapsack
    - Add TODO to knapsack
    - Fix bug with knapsack_wh

--- algorithms/dynamic/knapsack.py
+++ algorithms/dynamic/knapsack.py
@@ -1,15 +1,42 @@
+def knapsack_rec(weight, items):
+    return knapsack_wh(weight, items, len(items), set(), {})
+
+
+def knapsack_wh(weight, items, n, picked, cache):
+    result = 0
+    if n == 0 or weight == 0:
+        return 0
+    if (n, weight) in cache:
+        return cache[(n, weight)]
+    for ix, val in enumerate(items):
+        if ix in picked:
+            continue
+        iw, iv = items[ix]
+        if iw <= weight:
+            picked.add(ix)
+            used = iv + knapsack_wh(weight - iw, items, n - 1, picked, cache)
+            not_used = knapsack_wh(weight, items, n - 1, picked, cache)
+            result = max(result, used, not_used)
+            picked.remove(ix)
+        else:
+            not_used = knapsack_wh(weight, items, n - 1, picked, cache)
+            result = max(result, not_used)
+    cache[(n, weight)] = result
+    return result
+
+
 # https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
 def knapsack_dp(weight, items):
     N = len(items) + 1
     W = weight + 1
-    answers = [[0] * W for _ in range(N)]
+    total = [[0] * W for _ in range(N)]
     for i in range(1, N):
         for j in range(1, W):
             iw, iv = items[i - 1]
-            not_used = answers[i - 1][j]
+            not_used = total[i - 1][j]
             if iw <= j:
-                used = iv + answers[i - 1][j - iw]
-                answers[i][j] = max(used, not_used)
+                used = iv + total[i - 1][j - iw]
+                total[i][j] = max(used, not_used)
             else:
-                answers[i][j] = not_used
-    return answers[-1][-1]
+                total[i][j] = not_used
+    return total[-1][-1]
--- tests/dynamic/test_knapsack.py
+++ tests/dynamic/test_knapsack.py
@@ -1,12 +1,30 @@
 import pytest
 
-from algorithms.dynamic.knapsack import knapsack_dp
+from algorithms.dynamic import knapsack as knap
 
 
 @pytest.mark.dynamic
 class TestKnapsack:
-    def test_knapsack_dp(self):
+    def test_knapsack_dp_1(self):
         weight = 50
         items = [(10, 60), (20, 100), (30, 120)]
-        result = knapsack_dp(weight, items)
+        result = knap.knapsack_dp(weight, items)
         assert result == 220
+
+    def test_knapsack_dp_2(self):
+        weight = 20
+        items = [(7, 160), (3, 90), (2, 15)]
+        result = knap.knapsack_dp(weight, items)
+        assert result == 265
+
+    def test_knapsack_rec_1(self):
+        weight = 50
+        items = [(10, 60), (20, 100), (30, 120)]
+        result = knap.knapsack_rec(weight, items)
+        assert result == 220
+
+    def test_knapsack_rec_2(self):
+        weight = 20
+        items = [(7, 160), (3, 90), (2, 15)]
+        result = knap.knapsack_rec(weight, items)
+        assert result == 265
