1f23038ad85294f1487988cbe256fac835103bd3
Samuel Huang <samhuang91@gmail.com>
2019-05-29 21:21:52 -0700

    Modify main driver

--- algorithms/basic/word_wrap.py
+++ algorithms/basic/word_wrap.py
@@ -6,9 +6,7 @@ def word_wrap(words, length):
     # Step 1: Keep track of character counts as cursor moves
     cursor = 1
     chars_cur = length - len(words[0])
-    chars_prev = 0
     while chars_cur > 0 and cursor < len(words):
-        chars_prev = chars_cur
         chars_cur -= len(words[cursor]) + 1
         cursor += 1
 
@@ -19,17 +17,39 @@ def word_wrap(words, length):
 
     # Step 3: Inject proper amount of spacing for current content
     space_count = 0
-    while chars_prev > 0:
+    cur_total = sum([len(w) for w in cur_words])
+    chars_left = length - cur_total
+    while chars_left > 0:
         space_count += 1
-        chars_prev -= len(cur_words)
+        chars_left -= len(cur_words)
     spc_content = "_" * space_count
     cur_content = spc_content.join(words[1:pointer])
+
+    # Step 4: Add additional left-padding if needed
     buf_content = ""
     used_buffer = len(cur_content) + len(words[0])
     while len(buf_content) < length - used_buffer:
         buf_content += "_"
     cur_content = words[0] + buf_content + cur_content
 
-    # Step 4: Join current content with remaining content
+    # Step 5: Join current content with remaining content
     rem_content = word_wrap(rem_words, length)
     return cur_content + "\n" + rem_content if rem_content else cur_content
+
+
+if __name__ == "__main__":
+    words = [
+        "hello",
+        "world",
+        "t" * 6,
+        "needs",
+        "cleanup",
+        "for",
+        "a",
+        "while",
+        "longer",
+        "today",
+    ]
+    length = 20
+    result = word_wrap(words, length)
+    print(result)
