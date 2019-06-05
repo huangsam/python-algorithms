8c97301e0f513d7ab19f28e9f9f6aeb0002b5d3a
Samuel Huang <samhuang91@gmail.com>
2019-06-04 22:21:27 -0700

    Implement floyd_warshall

---
+++ algorithms/dynamic/floyd_warshall.py
@@ -0,0 +1,20 @@
+# https://www.geeksforgeeks.org/floyd-warshall-algorithm-dp-16/
+def floyd_warshall(graph):
+    V = len(graph)
+    dist = map(lambda i: map(lambda j: j, i), graph)
+    for k in range(V):
+        for i in range(V):
+            for j in range(V):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    return dist
+
+
+# https://www.geeksforgeeks.org/transitive-closure-of-a-graph/
+def transitive_closure(graph):
+    V = len(graph)
+    reach = map(i[:] for i in graph)
+    for k in range(V):
+        for i in range(V):
+            for j in range(V):
+                reach[i][j] = reach[i][j] or (reach[i][k] and reach[k][j])
+    return reach
