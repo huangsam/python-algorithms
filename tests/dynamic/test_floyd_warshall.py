6fe6adc14908b63c2c3bae597e4bba67ad86a7e9
Samuel Huang <samhuang91@gmail.com>
2019-06-04 22:54:43 -0700

    Refine test cases

--- tests/dynamic/test_floyd_warshall.py
+++ tests/dynamic/test_floyd_warshall.py
@@ -15,7 +15,18 @@ class TestFloydWarshall:
             [MAX_INT, MAX_INT, MAX_INT, 0],
         ]
         result = floyd.floyd_warshall(graph)
-        assert isinstance(result, list)
-        assert isinstance(result[0], list)
-        assert graph[0][3] == 10
-        assert result[0][3] == 9
+        assert result == [
+            [0, 5, 8, 9],
+            [MAX_INT, 0, 3, 4],
+            [MAX_INT, MAX_INT, 0, 1],
+            [MAX_INT, MAX_INT, MAX_INT, 0],
+        ]
+
+
+@pytest.mark.graph
+@pytest.mark.dynamic
+class TestTransitiveClosure:
+    def test_transitive_closure(self):
+        graph = [[1, 1, 0, 1], [0, 1, 1, 0], [0, 0, 1, 1], [0, 0, 0, 1]]
+        result = floyd.transitive_closure(graph)
+        assert result == [[1, 1, 1, 1], [0, 1, 1, 1], [0, 0, 1, 1], [0, 0, 0, 1]]
