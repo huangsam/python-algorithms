f82281095d3b2c885117919b550668b20009d751
Samuel Huang <samhuang91@gmail.com>
2019-06-18 08:39:01 -0700

    Add test cases

--- algorithms/basic/word_search.py
+++ algorithms/basic/word_search.py
@@ -1,9 +1,16 @@
 def naive(content, pattern):
     plen = len(pattern)
+    clen = len(content)
+    if plen == 0:
+        return -1
+    if clen < plen:
+        return -1
+    if clen == plen:
+        return 0 if content == pattern else -1
     for ix in range(plen, len(content)):
         if content[ix - plen : ix] == pattern:
-            return True
-    return False
+            return ix - plen
+    return -1
 
 
 # https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
