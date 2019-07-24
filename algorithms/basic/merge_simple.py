882cebe5ae6cea048df3d04aa772a818125177d8
Samuel Huang <samhuang91@gmail.com>
2019-07-23 21:43:24 -0700

    Make merge_arrays easier to read

--- algorithms/basic/merge_simple.py
+++ algorithms/basic/merge_simple.py
@@ -10,18 +10,11 @@ def merge_sort(arr):
 def merge_arrays(arr1, arr2):
     result = []
     a1, a2 = 0, 0
-    while True:
-        if a1 >= len(arr1):
-            result.extend(arr2[a2:])
-            break
-        elif a2 >= len(arr2):
-            result.extend(arr1[a1:])
-            break
+    while a1 < len(arr1) and a2 < len(arr2):
+        if arr1[a1] <= arr2[a2]:
+            result.append(arr1[a1])
+            a1 += 1
         else:
-            if arr1[a1] <= arr2[a2]:
-                result.append(arr1[a1])
-                a1 += 1
-            else:
-                result.append(arr2[a2])
-                a2 += 1
-    return result
+            result.append(arr2[a2])
+            a2 += 1
+    return [*result, *arr1[a1:], *arr2[a2:]]
