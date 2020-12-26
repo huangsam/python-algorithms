5772b2f95f077328ee445aa4ca851052ca86faa1
Samuel Huang <samhuang91@gmail.com>
2020-12-26 00:33:56 -0800

    Rename import for test_isTreeSymmetric

--- tests/online/codesignal/test_isTreeSymmetric.py
+++ tests/online/codesignal/test_isTreeSymmetric.py
@@ -1,7 +1,7 @@
 import pytest
 
 from algorithms.collection.tree import TreeNode
-from algorithms.online.codesignal import isTreeSymmetric as symmetry
+from algorithms.online.codesignal import isTreeSymmetric as symmetric
 
 
 @pytest.mark.tree
@@ -13,7 +13,7 @@ def test_symmetric_good():
     root.right = TreeNode(2)
     root.right.left = TreeNode(4)
     root.right.right = TreeNode(3)
-    assert symmetry.isTreeSymmetric(root)
+    assert symmetric.isTreeSymmetric(root)
 
 
 @pytest.mark.tree
@@ -23,4 +23,4 @@ def test_symmetric_bad():
     root.left.right = TreeNode(3)
     root.right = TreeNode(2)
     root.right.right = TreeNode(3)
-    assert not symmetry.isTreeSymmetric(root)
+    assert not symmetric.isTreeSymmetric(root)
