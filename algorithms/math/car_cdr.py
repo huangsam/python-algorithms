57047f82b205aef9758a34cc4be954e101b47618
Sam Huang <samhuang91@gmail.com>
2020-12-26 00:41:17 -0800

    Refine car_cdr semantics

--- algorithms/math/car_cdr.py
+++ algorithms/math/car_cdr.py
@@ -1,12 +1,12 @@
 def car(pair):
-    def get_first(x, y):
+    def get_first(x, _):
         return x
 
     return pair(get_first)
 
 
 def cdr(pair):
-    def get_last(x, y):
+    def get_last(_, y):
         return y
 
     return pair(get_last)
