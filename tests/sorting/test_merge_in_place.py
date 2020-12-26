1f721c92858ed0d279915a479e52b428ba7407d2
Sam Huang <samhuang91@gmail.com>
2020-12-25 21:34:41 -0800

    Convert sorting methods to functions

--- tests/sorting/test_merge_in_place.py
+++ tests/sorting/test_merge_in_place.py
@@ -6,33 +6,44 @@ from tests.utils import is_sorted
 
 @pytest.mark.array
 @pytest.mark.sorting
-class TestMergeInPlace:
-    def test_merge_in_place_a(self):
-        a = [1, 2]
-        b = []
-        mip.merge_in_place(a, b)
-        assert is_sorted(a)
+def test_merge_in_place_a():
+    a = [1, 2]
+    b = []
+    mip.merge_in_place(a, b)
+    assert is_sorted(a)
 
-    def test_merge_in_place_b(self):
-        a = [None, None]
-        b = [1, 2]
-        mip.merge_in_place(a, b)
-        assert is_sorted(a)
 
-    def test_merge_in_place_unique(self):
-        a = [1, 2, 3, None, None, None]
-        b = [4, 5, 6]
-        mip.merge_in_place(a, b)
-        assert is_sorted(a)
+@pytest.mark.array
+@pytest.mark.sorting
+def test_merge_in_place_b():
+    a = [None, None]
+    b = [1, 2]
+    mip.merge_in_place(a, b)
+    assert is_sorted(a)
 
-    def test_merge_in_place_zigzag(self):
-        a = [1, 3, 5, None, None, None]
-        b = [2, 4, 6]
-        mip.merge_in_place(a, b)
-        assert is_sorted(a)
 
-    def test_merge_in_place_big(self):
-        a = [1, 3, 5, 8, 10, 14, None, None, None, None, None]
-        b = [2, 4, 6, 9, 11]
-        mip.merge_in_place(a, b)
-        assert is_sorted(a)
+@pytest.mark.array
+@pytest.mark.sorting
+def test_merge_in_place_unique():
+    a = [1, 2, 3, None, None, None]
+    b = [4, 5, 6]
+    mip.merge_in_place(a, b)
+    assert is_sorted(a)
+
+
+@pytest.mark.array
+@pytest.mark.sorting
+def test_merge_in_place_zigzag():
+    a = [1, 3, 5, None, None, None]
+    b = [2, 4, 6]
+    mip.merge_in_place(a, b)
+    assert is_sorted(a)
+
+
+@pytest.mark.array
+@pytest.mark.sorting
+def test_merge_in_place_big():
+    a = [1, 3, 5, 8, 10, 14, None, None, None, None, None]
+    b = [2, 4, 6, 9, 11]
+    mip.merge_in_place(a, b)
+    assert is_sorted(a)
--- tests/sorting/test_selection_sort.py
+++ tests/sorting/test_selection_sort.py
@@ -6,8 +6,7 @@ from tests.utils import is_sorted
 
 @pytest.mark.array
 @pytest.mark.sorting
-class TestSelectionSort:
-    def test_selection_sort(self, array):
-        assert not is_sorted(array)
-        selection_sort.sort(array)
-        assert is_sorted(array)
+def test_selection_sort(array):
+    assert not is_sorted(array)
+    selection_sort.sort(array)
+    assert is_sorted(array)
