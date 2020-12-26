2e171c54a6b84b281bdf8885ca7a33936226770e
Sam Huang <samhuang91@gmail.com>
2020-12-25 21:21:26 -0800

    Convert collection methods to functions

--- tests/collection/test_graph.py
+++ tests/collection/test_graph.py
@@ -6,69 +6,87 @@ SAMPLE_DATA = (("a", "b"), ("a", "c"), ("a", "d"))
 
 
 @pytest.mark.graph
-class TestDirectedGraph:
-    def test_setup_directed(self):
-        graph = DirectedGraph(*SAMPLE_DATA)
-        assert len(graph.get_nodes()) == 4
-
-    def test_add_edge_directed(self):
-        graph = DirectedGraph()
-        for src, dst in SAMPLE_DATA:
-            graph.add_edge(src, dst)
-        assert len(graph.get_nodes()) == 4
-
-    def test_get_children_directed(self):
-        graph = DirectedGraph(*SAMPLE_DATA)
-        children = graph.get_children("a")
-        assert len(children) == len(SAMPLE_DATA)
-        assert "b" in children
-        assert "c" in children
-
-    def test_get_out_degree_directed(self):
-        graph = DirectedGraph(*SAMPLE_DATA)
-        assert graph.get_out_degree("a") == 3
-        assert graph.get_out_degree("d") == 0
-
-    def test_get_in_degree_directed(self):
-        graph = DirectedGraph(*SAMPLE_DATA)
-        assert graph.get_in_degree("a") == 0
-        for child in graph.get_children("a"):
-            assert graph.get_in_degree(child) == 1
-
-    def test_check_node_directed(self):
-        graph = DirectedGraph(*SAMPLE_DATA)
-        for node in graph.get_nodes():
-            assert graph.check_node(node)
-
-    def test_check_edge_directed(self):
-        graph = DirectedGraph(*SAMPLE_DATA)
-        assert graph.check_edge(("a", "b"))
-        assert not graph.check_edge(("b", "a"))
+def test_setup_directed():
+    graph = DirectedGraph(*SAMPLE_DATA)
+    assert len(graph.get_nodes()) == 4
 
 
 @pytest.mark.graph
-class TestUndirectedGraph:
-    def test_setup_undirected(self):
-        graph = UndirectedGraph(*SAMPLE_DATA)
-        assert len(graph.get_nodes()) == 4
+def test_add_edge_directed():
+    graph = DirectedGraph()
+    for src, dst in SAMPLE_DATA:
+        graph.add_edge(src, dst)
+    assert len(graph.get_nodes()) == 4
 
-    def test_get_out_degree_undirected(self):
-        graph = UndirectedGraph(*SAMPLE_DATA)
-        assert graph.get_out_degree("a") == 3
-        assert graph.get_out_degree("d") == 1
 
-    def test_get_in_degree_undirected(self):
-        graph = UndirectedGraph(*SAMPLE_DATA)
-        assert graph.get_in_degree("a") == 3
-        for child in graph.get_children("a"):
-            assert graph.get_in_degree(child) == 1
+@pytest.mark.graph
+def test_get_children_directed():
+    graph = DirectedGraph(*SAMPLE_DATA)
+    children = graph.get_children("a")
+    assert len(children) == len(SAMPLE_DATA)
+    assert "b" in children
+    assert "c" in children
 
-    def test_check_node_undirected(self):
-        graph = UndirectedGraph(*SAMPLE_DATA)
-        for node in graph.get_nodes():
-            assert graph.check_node(node)
 
-    def test_check_edge_undirected(self):
-        graph = UndirectedGraph(*SAMPLE_DATA)
-        assert graph.check_edge(("a", "b"))
-        assert graph.check_edge(("b", "a"))
+@pytest.mark.graph
+def test_get_out_degree_directed():
+    graph = DirectedGraph(*SAMPLE_DATA)
+    assert graph.get_out_degree("a") == 3
+    assert graph.get_out_degree("d") == 0
+
+
+@pytest.mark.graph
+def test_get_in_degree_directed():
+    graph = DirectedGraph(*SAMPLE_DATA)
+    assert graph.get_in_degree("a") == 0
+    for child in graph.get_children("a"):
+        assert graph.get_in_degree(child) == 1
+
+
+@pytest.mark.graph
+def test_check_node_directed():
+    graph = DirectedGraph(*SAMPLE_DATA)
+    for node in graph.get_nodes():
+        assert graph.check_node(node)
+
+
+@pytest.mark.graph
+def test_check_edge_directed():
+    graph = DirectedGraph(*SAMPLE_DATA)
+    assert graph.check_edge(("a", "b"))
+    assert not graph.check_edge(("b", "a"))
+
+
+@pytest.mark.graph
+def test_setup_undirected():
+    graph = UndirectedGraph(*SAMPLE_DATA)
+    assert len(graph.get_nodes()) == 4
+
+
+@pytest.mark.graph
+def test_get_out_degree_undirected():
+    graph = UndirectedGraph(*SAMPLE_DATA)
+    assert graph.get_out_degree("a") == 3
+    assert graph.get_out_degree("d") == 1
+
+
+@pytest.mark.graph
+def test_get_in_degree_undirected():
+    graph = UndirectedGraph(*SAMPLE_DATA)
+    assert graph.get_in_degree("a") == 3
+    for child in graph.get_children("a"):
+        assert graph.get_in_degree(child) == 1
+
+
+@pytest.mark.graph
+def test_check_node_undirected():
+    graph = UndirectedGraph(*SAMPLE_DATA)
+    for node in graph.get_nodes():
+        assert graph.check_node(node)
+
+
+@pytest.mark.graph
+def test_check_edge_undirected():
+    graph = UndirectedGraph(*SAMPLE_DATA)
+    assert graph.check_edge(("a", "b"))
+    assert graph.check_edge(("b", "a"))
--- tests/collection/test_stack.py
+++ tests/collection/test_stack.py
@@ -4,64 +4,74 @@ from algorithms.collection.stack import FunkyStack, Stack
 
 
 @pytest.mark.stack
-class TestStack:
-    def test_push(self):
-        stack = Stack()
-        stack.push(1)
-        stack.push(2)
-
-    def test_pop(self):
-        stack = Stack()
-        stack.push(1)
-        stack.push(2)
-        assert stack.pop() == 2
-
-    def test_max_push(self):
-        stack = Stack()
-        stack.push(4)
-        stack.push(3)
-        assert stack.max() == 4
-
-    def test_max_pop(self):
-        stack = Stack()
-        stack.push(4)
-        stack.push(5)
-        assert stack.max() == 5
-        stack.push(5)
-        stack.pop()
-        assert stack.max() == 5
-        stack.pop()
-        assert stack.max() == 4
-
-    def test_max_pop_error(self):
-        stack = Stack()
-        stack.push(4)
-        assert stack.max() == 4
-        stack.pop()
-        with pytest.raises(ValueError):
-            stack.max()
-
-    def test_size(self):
-        stack = Stack()
-        for i in range(100):
-            stack.push(i)
-        for i in range(20):
-            stack.pop()
-        assert stack.size() == 80
+def test_push():
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
 
 
 @pytest.mark.stack
-class TestFunkyStack:
-    def test_push(self):
-        stack = FunkyStack()
-        stack.push(1)
-        stack.push(2)
+def test_pop():
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    assert stack.pop() == 2
 
-    def test_pop(self):
-        stack = FunkyStack()
-        stack.push(1)
-        stack.push(2)
-        assert stack.pop() == 2
-        assert stack.size() == 1
-        assert stack.pop() == 1
-        assert stack.size() == 0
+
+@pytest.mark.stack
+def test_max_push():
+    stack = Stack()
+    stack.push(4)
+    stack.push(3)
+    assert stack.max() == 4
+
+
+@pytest.mark.stack
+def test_max_pop():
+    stack = Stack()
+    stack.push(4)
+    stack.push(5)
+    assert stack.max() == 5
+    stack.push(5)
+    stack.pop()
+    assert stack.max() == 5
+    stack.pop()
+    assert stack.max() == 4
+
+
+@pytest.mark.stack
+def test_max_pop_error():
+    stack = Stack()
+    stack.push(4)
+    assert stack.max() == 4
+    stack.pop()
+    with pytest.raises(ValueError):
+        stack.max()
+
+
+@pytest.mark.stack
+def test_size():
+    stack = Stack()
+    for i in range(100):
+        stack.push(i)
+    for i in range(20):
+        stack.pop()
+    assert stack.size() == 80
+
+
+@pytest.mark.stack
+def test_push():
+    stack = FunkyStack()
+    stack.push(1)
+    stack.push(2)
+
+
+@pytest.mark.stack
+def test_pop():
+    stack = FunkyStack()
+    stack.push(1)
+    stack.push(2)
+    assert stack.pop() == 2
+    assert stack.size() == 1
+    assert stack.pop() == 1
+    assert stack.size() == 0
