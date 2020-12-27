2320b14e6727522dc204f964f60926b946c62f5e
Samuel Huang <samhuang91@gmail.com>
2020-12-26 16:36:15 -0800

    Add type hinting to climb_steps

--- tests/dynamic/test_climb_steps.py
+++ tests/dynamic/test_climb_steps.py
@@ -4,6 +4,6 @@ from algorithms.dynamic.climb_steps import climb_steps
 
 
 @pytest.mark.dynamic
-@pytest.mark.parametrize("levels, steps, expected", [(4, (1, 2), 5), (4, (1, 3, 5), 3)])
+@pytest.mark.parametrize("levels, steps, expected", [(4, [1, 2], 5), (4, [1, 3, 5], 3)])
 def test_climb_steps(levels, steps, expected):
     assert climb_steps(levels, steps) == expected
