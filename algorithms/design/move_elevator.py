7cc52941fb0accb5a3fb6067e2a9e201e0dbe6b3
Samuel Huang <samhuang91@gmail.com>
2020-12-27 14:46:17 -0800

    Optimize routines in move_elevator

--- algorithms/design/move_elevator.py
+++ algorithms/design/move_elevator.py
@@ -1,4 +1,5 @@
 from heapq import heappop, heappush
+from typing import Tuple
 
 
 def move_elevator(current_level, op_queue, direction):
@@ -9,12 +10,12 @@ def move_elevator(current_level, op_queue, direction):
     priority = []
     visited = set()
     print("== {dir} from level {cur}".format(dir=direction, cur=current_level))
-    bound = get_bound(current_level, op_queue, direction)
+    bound = _get_bound(current_level, op_queue, direction)
     for pair in op_queue:
         pair_left, pair_right = pair
-        if spans(bound, pair) and valid(pair, direction):
-            heappush(priority, (cost(current_level, pair_left), pair_left))
-            heappush(priority, (cost(current_level, pair_right), pair_right))
+        if _spans(bound, pair) and _valid(pair, direction):
+            heappush(priority, (_cost(current_level, pair_left), pair_left))
+            heappush(priority, (_cost(current_level, pair_right), pair_right))
             visited.add(pair)
     op_queue = op_queue.difference(visited)
     while len(priority) > 0:
@@ -22,24 +23,24 @@ def move_elevator(current_level, op_queue, direction):
         print("Arrived at this level: {nxt}".format(nxt=next_level))
     if len(op_queue) > 0:
         if next_level:
-            move_elevator(next_level, op_queue, opposite(direction))
+            move_elevator(next_level, op_queue, _opposite(direction))
         else:
             left, right = op_queue.pop()
             op_queue.add((left, right))
-            move_elevator(left, op_queue, opposite(direction))
+            move_elevator(left, op_queue, _opposite(direction))
 
 
-def get_bound(current_level, op_queue, direction):
+def _get_bound(current_level, op_queue, direction) -> Tuple[int, int]:
     """Calculate boundary given remaining operations."""
     if direction.upper() == "UP":
-        max_level = get_max_level(op_queue)
+        max_level = _get_max_level(op_queue)
         return current_level, max_level
     elif direction.upper() == "DOWN":
-        min_level = get_min_level(op_queue)
+        min_level = _get_min_level(op_queue)
         return min_level, current_level
 
 
-def valid(pair, direction):
+def _valid(pair, direction) -> bool:
     """Determine whether pair is valid given direction."""
     if direction.upper() == "UP":
         return pair[0] < pair[1]
@@ -47,46 +48,31 @@ def valid(pair, direction):
         return pair[0] > pair[1]
 
 
-def cost(current, target):
+def _cost(current, target) -> int:
     """Determine cost of entering target."""
     return abs(target - current)
 
 
-def get_max_level(op_queue):
+def _get_max_level(op_queue) -> int:
     """Get maximum level for remaining operations."""
-    result = None
-    for i, j in op_queue:
-        if result is None:
-            result = max(i, j)
-        else:
-            result = max(result, i, j)
-    return result
+    return max(v for op in op_queue for v in op)
 
 
-def get_min_level(op_queue):
+def _get_min_level(op_queue) -> int:
     """Get minimum level for remaining operations."""
-    result = None
-    for i, j in op_queue:
-        if result is None:
-            result = min(i, j)
-        else:
-            result = min(result, i, j)
-    return result
+    return min(v for op in op_queue for v in op)
 
 
-def spans(bound, pair):
+def _spans(bound, pair) -> bool:
     """Determine whether boundary spans pair."""
     bl, br = bound
     pl, pr = pair
     return bl <= pl <= br and bl <= pr <= br
 
 
-def opposite(direction):
+def _opposite(direction) -> str:
     """Simple toggle for direction."""
-    if direction.upper() == "UP":
-        return "DOWN"
-    else:
-        return "UP"
+    return "DOWN" if direction.upper() == "UP" else "UP"
 
 
 def main():
