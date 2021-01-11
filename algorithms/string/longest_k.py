8bffc7df716a742efa9011e523a10fe8e19468c1
Samuel Huang <samhuang91@gmail.com>
2021-01-10 22:12:54 -0800

    Rename functions in string

--- algorithms/string/longest_k.py
+++ algorithms/string/longest_k.py
@@ -1,16 +1,15 @@
 def longest_k_distinct(s, k):
     if len(s) == 0:
         return ""
-    if is_valid(s, k) is True:
+    if _is_valid(s, k) is True:
         return s
     left = longest_k_distinct(s[1:], k)
     right = longest_k_distinct(s[:-1], k)
     return left if len(left) > len(right) else right
 
 
-def is_valid(s, k):
-    unique = set(list(s))
-    return len(unique) == k
+def _is_valid(s, k):
+    return len(set(s)) == k
 
 
 # https://www.geeksforgeeks.org/find-the-longest-substring-with-k-unique-characters-in-a-given-string/
@@ -33,7 +32,7 @@ def longest_k_distinct_optimal(s, k):
             counts[val] = 0
         counts[val] += 1
         end += 1
-        while is_valid_optimal(counts, k) is False:
+        while _is_valid_optimal(counts, k) is False:
             sval = s[start]
             counts[sval] -= 1
             if counts[sval] == 0:
@@ -45,5 +44,5 @@ def longest_k_distinct_optimal(s, k):
     return s[result_start : (result_start + result_size)]
 
 
-def is_valid_optimal(counts, k):
+def _is_valid_optimal(counts, k):
     return len(counts) <= k
