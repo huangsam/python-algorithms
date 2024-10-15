4f9d7f2461f238c88736a161425793513a6da272
Samuel Huang <samhuang91@gmail.com>
2024-10-15 00:38:54 -0700

    Use enum in place of strings

--- algorithms/tree/remove_bad_edge.py
+++ algorithms/tree/remove_bad_edge.py
@@ -1,32 +1,39 @@
+from enum import Enum, auto
+
 from algorithms.collections.tree import TreeNode
+
+
+class Direction(Enum):
+    LEFT = auto()
+    RIGHT = auto()
 
 
 def remove_bad_edge(root: TreeNode):
     seen: set = set()
     cur: TreeNode | None = root
     par: TreeNode | None = None
-    direction: str | None = None
+    direction: Direction | None = None
     _remove_bad_edge_work(cur, par, direction, seen)
 
 
 def _remove_bad_edge_work(
     cur: TreeNode | None,
     par: TreeNode | None,
-    direction: str | None,
+    direction: Direction | None,
     seen: set,
 ):
     if cur is None:
         return
 
     if id(cur) in seen:
-        if direction == "l" and par:
+        if direction == Direction.LEFT and par:
             par.left = None
             return
-        if direction == "r" and par:
+        if direction == Direction.RIGHT and par:
             par.right = None
             return
     else:
         seen.add(id(cur))
 
-    _remove_bad_edge_work(cur.left, cur, "l", seen)
-    _remove_bad_edge_work(cur.right, cur, "r", seen)
+    _remove_bad_edge_work(cur.left, cur, Direction.LEFT, seen)
+    _remove_bad_edge_work(cur.right, cur, Direction.RIGHT, seen)
