1300e9297f68e4580ac47d075ecfb6cd5c635499
Samuel Huang <samhuang91@gmail.com>
2024-10-14 23:39:04 -0700

    Refactor SO link in constants

--- algorithms/constants.py
+++ algorithms/constants.py
@@ -1,6 +1,6 @@
 import sys
 
 # Maximum and minimum values for canonical integer
-# https://stackoverflow.com/questions/7604966/maximum-and-minimum-values-for-ints
+# https://stackoverflow.com/a/7604981
 MAX_INT: int = sys.maxsize
 MIN_INT: int = -sys.maxsize - 1
