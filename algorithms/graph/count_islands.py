705edcf5f596b49aa82f358cba7e267d333bf9ce
Samuel Huang <samhuang91@gmail.com>
2024-11-17 22:28:15 -0800

    Add type hinting for count_islands

--- algorithms/graph/count_islands.py
+++ algorithms/graph/count_islands.py
@@ -1,5 +1,5 @@
 # https://www.geeksforgeeks.org/find-number-of-islands/
-def count_islands(grid):
+def count_islands(grid: list[list[int]]):
     result = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
@@ -9,7 +9,7 @@ def count_islands(grid):
     return result
 
 
-def _visit_island(i, j, grid):
+def _visit_island(i: int, j: int, grid: list[list[int]]):
     if i < 0 or j < 0:
         return
     elif i >= len(grid) or j >= len(grid[0]):
